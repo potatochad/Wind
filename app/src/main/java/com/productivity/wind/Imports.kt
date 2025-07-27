1bf60e74b245506f75e229b896d064ec35a4f065
potatochad <163626408+potatochad@users.noreply.github.com>
2025-07-27 09:50:01 +0200

    Update Imports.kt

--- app/src/main/java/com/productivity/wind/Imports.kt
+++ app/src/main/java/com/productivity/wind/Imports.kt
@@ -137,6 +137,7 @@ import android.widget.ScrollView
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.TextFieldDefaults
 
 
 
