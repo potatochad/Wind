1bffaf05b8bd46520c7119332c38be11be7c345b
potatochad <163626408+potatochad@users.noreply.github.com>
2025-08-03 14:19:29 +0200

    Update Data.kt

--- app/src/main/java/com/productivity/wind/Data.kt
+++ app/src/main/java/com/productivity/wind/Data.kt
@@ -1,5 +1,6 @@
 package com.productivity.wind
 
+import androidx.compose.runtime.snapshots.*
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import androidx.compose.foundation.verticalScroll
