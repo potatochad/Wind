2721ed188a785bf7f671eb283f897d51981b9a5c
potatochad <163626408+potatochad@users.noreply.github.com>
2025-08-10 20:46:01 +0300

    Update Imports.kt

--- app/src/main/java/com/productivity/wind/Imports.kt
+++ app/src/main/java/com/productivity/wind/Imports.kt
@@ -1147,7 +1147,7 @@ fun settingsHeader(
             modifier = modifier
                 .fillMaxWidth()
                 .background(Color.Black)
-                .padding(horizontal = 4.dp, vertical = 12.dp)
+                .padding(vertical = 12.dp)
 			.heightIn(max = Mheight.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
