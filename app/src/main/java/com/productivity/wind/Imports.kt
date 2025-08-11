1f8c092dab7ca0c1bccfd784b1888085153de7a7
potatochad <163626408+potatochad@users.noreply.github.com>
2025-08-11 14:52:44 +0300

    Update Imports.kt

--- app/src/main/java/com/productivity/wind/Imports.kt
+++ app/src/main/java/com/productivity/wind/Imports.kt
@@ -1465,6 +1465,32 @@ fun LazyMenu(
 
 
 
+
+object DayChecker {
+    private var job: Job? = null
+
+        
+    fun start() {
+        if (job?.isActive == true) return  // Already running
+        if (Bar.lastDate == "") { Bar.lastDate = LocalDate.now().toString() }
+            
+        job = CoroutineScope(Dispatchers.Default).launch {
+            while (coroutineContext.isActive) {
+                delay(60 * 1000L)
+                val today = LocalDate.now().toString()
+                if (today != Bar.lastDate) {
+                    Bar.lastDate = today
+                    onNewDay()
+                }
+            }
+        }
+    }
+
+    
+    
+}
+
+
 //endregion
 
 //endregion
