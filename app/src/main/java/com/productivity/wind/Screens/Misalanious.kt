3bd7cfbfbc795237e05bcbdbfa91ef416b6f398c
potatochad <163626408+potatochad@users.noreply.github.com>
2025-08-11 19:59:01 +0300

    Update Misalanious.kt

--- app/src/main/java/com/productivity/wind/Screens/Misalanious.kt
+++ app/src/main/java/com/productivity/wind/Screens/Misalanious.kt
@@ -87,13 +87,12 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-
-
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
+import androidx.navigation.NavGraphBuilder
 
 //region NavController
 //Global1.navController - to use anywhere, no input
