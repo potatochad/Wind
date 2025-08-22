f54060ff74be27272873ac24e978b37850560be2
potatochad <163626408+potatochad@users.noreply.github.com>
2025-08-22 08:50:15 +0300

    Update MainUI.kt

--- app/src/main/java/com/productivity/wind/Screens/MainUI.kt
+++ app/src/main/java/com/productivity/wind/Screens/MainUI.kt
@@ -43,7 +43,7 @@ fun Main() {
     LazyMenu { Menu() }
     MAINStart()
     
-    LazyScreen(titleContent = { MainHeader() }, showBack = false) {
+    LazyScreen( title ={ MainHeader() }, showBack = false) {
         
                 LazyCard{ Disipline() } 
                 LazyCard{ German() }
