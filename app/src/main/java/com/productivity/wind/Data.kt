5150e7dc8979547e083a8a0b63c8e2d4b4690ba5
potatochad <163626408+potatochad@users.noreply.github.com>
2025-08-23 11:55:24 +0300

    Update Data.kt

--- app/src/main/java/com/productivity/wind/Data.kt
+++ app/src/main/java/com/productivity/wind/Data.kt
@@ -279,6 +279,7 @@ fun AppStart() {
     LaunchedEffect(Unit) {
         refreshApps()
     }
+    LazyMenu { Menu() }
     
     PopUps()
     val halfWidth = LocalConfiguration.current.screenWidthDp.dp/2+30.dp; Bar.halfWidth = halfWidth
