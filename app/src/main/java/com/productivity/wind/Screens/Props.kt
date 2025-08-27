221a444fd257ad304a6ea261abb097afe65652cf
potatochad <163626408+potatochad@users.noreply.github.com>
2025-08-27 21:19:56 +0300

    Update Props.kt

--- app/src/main/java/com/productivity/wind/Screens/Props.kt
+++ app/src/main/java/com/productivity/wind/Screens/Props.kt
@@ -169,14 +169,6 @@ object Icon {
                         icon = Icons.Default.Add
                 )
         }
-        @Composable
-        fun BigAdd(onClick: () -> Unit = { goTo("Challenge") }) {
-                LazyIcon(
-                    onClick = onClick,
-                    BigIcon = Icons.Filled.Add,
-                    BigIconColor = Color(0xFFFFC107),
-                )
-        }
         
         @Composable
         fun Edit() {
