3166301faa637f01337f041cbd21c591cf20f4ee
potatochad <163626408+potatochad@users.noreply.github.com>
2025-09-01 20:01:22 +0300

    Update Imports.kt

--- app/src/main/java/com/productivity/wind/Imports/Imports.kt
+++ app/src/main/java/com/productivity/wind/Imports/Imports.kt
@@ -90,6 +90,7 @@ import com.productivity.wind.R
 import android.webkit.*
 import androidx.compose.foundation.shape.*
 import androidx.compose.ui.graphics.painter.*
+import androidx.compose.ui.graphics.painter.rememberDrawablePainter
 
 
 
