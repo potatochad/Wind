9d898d7e03348822db02e74687bce910e486380c
potatochad <163626408+potatochad@users.noreply.github.com>
2025-09-01 19:10:15 +0300

    Update web.kt

--- app/src/main/java/com/productivity/wind/Screens/web.kt
+++ app/src/main/java/com/productivity/wind/Screens/web.kt
@@ -18,25 +18,17 @@ import org.mozilla.geckoview.*
 import android.content.*
 
 @Composable
-fun Web2() {
-    var url = r { m("https://www.google.com") }
-        
+fun Web() {
+    var url by r { m("https://www.google.com") }
+    // Create Gecko runtime once
+
+    // Show GeckoView in Compose
     LazyScreen(
         title = {
             UI.Cinput(what=url, WidthMax=150)
         },
     ) {
-        //LazyBrowser(url)
-    }
-}
-
-
-
-@Composable
-fun Web() {
-    var url by r { m("https://www.google.com") }
-    // Create Gecko runtime once
-    val ctx = androidx.compose.ui.platform.LocalContext.current
+        val ctx = androidx.compose.ui.platform.LocalContext.current
     val geckoRuntime = r { GeckoRuntime.create(ctx) }
     val geckoSession = r {
         GeckoSession().apply {
@@ -44,20 +36,20 @@ fun Web() {
             loadUri(url)
         }
     }
-
-    // Show GeckoView in Compose
-    AndroidView(
-        factory = { ctx: Context ->
-            GeckoView(ctx).apply {
-                setSession(geckoSession)
+        AndroidView(
+            factory = { ctx: Context ->
+                GeckoView(ctx).apply {
+                    setSession(geckoSession)
+                }
+            },
+            update = { view ->
+                // If you want to reload when url changes
+                view.session?.loadUri(url)
             }
-        },
-        update = { view ->
-            // If you want to reload when url changes
-            view.session?.loadUri(url)
-        }
-    )
+        )
+    }
 }
+
 
 
 
