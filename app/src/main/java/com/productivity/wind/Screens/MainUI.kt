fe03370608f1d2182390e77df16833c88c9af877
potatochad <163626408+potatochad@users.noreply.github.com>
2025-09-03 12:14:26 +0300

    Update MainUI.kt

--- app/src/main/java/com/productivity/wind/Screens/MainUI.kt
+++ app/src/main/java/com/productivity/wind/Screens/MainUI.kt
@@ -70,6 +70,7 @@ fun Main() {
                     if (app.NowTime > app.DoneTime - 1 && !app.done) {
                         Bar.funTime += app.Worth
                         app.done = true
+                        Vlog("${app.name} completed")
                     }
                     
                     LazyCard {
