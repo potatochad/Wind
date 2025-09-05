c7791ad0db0e357ce9da4b36191e59352812c901
potatochad <163626408+potatochad@users.noreply.github.com>
2025-09-05 09:27:22 +0200

    Update web.kt

--- app/src/main/java/com/productivity/wind/Screens/web.kt
+++ app/src/main/java/com/productivity/wind/Screens/web.kt
@@ -45,7 +45,7 @@ fun Web() {
         AndroidView(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(600.dp), // fixed height inside lazy container
+                .height(900.dp), // fixed height inside lazy container
             factory = { context ->
                 GeckoView(context).apply {
                     setSession(geckoSession)
