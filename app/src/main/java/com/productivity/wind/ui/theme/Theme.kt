1ae3bba9d650723db1f4274b14df7dbd47f3034a
potatochad <163626408+potatochad@users.noreply.github.com>
2025-09-07 14:34:49 +0200

    Update Theme.kt

--- app/src/main/java/com/productivity/wind/ui/theme/Theme.kt
+++ app/src/main/java/com/productivity/wind/ui/theme/Theme.kt
@@ -4,6 +4,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.foundation.text.selection.*
 
 private val AppColorScheme = darkColorScheme(
     background = Color.Black,
