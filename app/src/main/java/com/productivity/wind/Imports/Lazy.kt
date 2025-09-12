a6b661baf7426cf3fcc622871dd901ec34af9bd9
potatochad <163626408+potatochad@users.noreply.github.com>
2025-09-12 10:51:24 +0200

    Update Lazy.kt

--- app/src/main/java/com/productivity/wind/Imports/Lazy.kt
+++ app/src/main/java/com/productivity/wind/Imports/Lazy.kt
@@ -245,7 +245,7 @@ fun LazyInfo(
     }
 
     // Default top-right of the trigger
-	var Y_Placement = if (map.up+10.dp < popupHeight) map.bottom+10.dp else map.up +10.dp
+	var Y_Placement = if (map.top +10.dp < popupHeight) map.bottom+10.dp else map.top +10.dp
 	var X_Placement = if (map.left < popupWidth) map.right else map.left
 	
 	if (show.value) Vlog("Popup at: Y=$Y_Placement X=$X_Placement")
