a5e666261f536407f0e494e073dd00e5e9ae3c80
potatochad <163626408+potatochad@users.noreply.github.com>
2025-09-13 10:20:45 +0200

    Update Data.kt

--- app/src/main/java/com/productivity/wind/Data.kt
+++ app/src/main/java/com/productivity/wind/Data.kt
@@ -29,6 +29,7 @@ import androidx.core.view.*
 import android.graphics.*
 import androidx.compose.foundation.layout.*
 import com.productivity.wind.Imports.*
+import androidx.compose.foundation.*
 
 /*! NEVER move bar and lists to another FOLDER, or other file
 aka....got some functions in datatools, that though a bit tantrum...
