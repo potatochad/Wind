ab3c1a5ce4fd2e6347a9feeabd7085fbc418272f
potatochad <163626408+potatochad@users.noreply.github.com>
2025-10-01 07:37:01 +0200

    Update Lazy.kt

--- app/src/main/java/com/productivity/wind/Imports/Lazy.kt
+++ app/src/main/java/com/productivity/wind/Imports/Lazy.kt
@@ -20,9 +20,7 @@ import androidx.compose.ui.platform.*
 import androidx.compose.ui.text.font.*
 import kotlinx.coroutines.*
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Divider
-import androidx.compose.material3.TextButton
+import androidx.compose.material3.*
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.window.Popup
@@ -30,21 +28,14 @@ import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlin.collections.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import androidx.compose.foundation.Image
@@ -67,6 +58,7 @@ import androidx.compose.ui.window.*
 import kotlin.math.*
 import androidx.compose.ui.unit.*
 import androidx.compose.ui.layout.*
+import androidx.compose.foundation.text.selection.*
 
 
 fun Modifier.clickOrHold(
@@ -108,9 +100,35 @@ fun LazyMove(
 		}
     }
 }
+
+
+
+
+
 @Composable
 fun LazyTheme(content: Content) {
-	
+    val appColorScheme = darkColorScheme(
+        background = Color.Black,
+        onBackground = Color.White,
+        surface = Color.Black,
+        onSurface = Color.White,
+        primary = Color.Black,
+        onPrimary = Color.White
+    )
+
+    val blueTextSelectionColors = TextSelectionColors(
+        handleColor = Color.Blue,                 
+        backgroundColor = Color.Blue.copy(alpha = 0.4f)  
+    )
+
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides blueTextSelectionColors
+    ) {
+        MaterialTheme(
+            colorScheme = appColorScheme,
+            content = content
+        )
+    }
 }
 
 
