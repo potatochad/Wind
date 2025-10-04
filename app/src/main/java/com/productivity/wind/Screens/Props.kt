0b14c6163192ac5101c2b82cad484e63ca76bd01
potatochad <163626408+potatochad@users.noreply.github.com>
2025-10-04 17:50:59 +0200

    Update Props.kt

--- app/src/main/java/com/productivity/wind/Screens/Props.kt
+++ app/src/main/java/com/productivity/wind/Screens/Props.kt
@@ -286,7 +286,7 @@ object Icon {
         @Composable
         fun Reload(webView: m_<WebView?>) {
             LazyIcon(
-                onClick = { webView?.it.reload() },
+                onClick = { webView.it?.reload() },
                 icon = Icons.Default.Refresh
             )
         }
