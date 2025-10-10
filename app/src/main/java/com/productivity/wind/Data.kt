c71aa3e71f9aeaade65a05112a04f8df88ea1bd9
potatochad <163626408+potatochad@users.noreply.github.com>
2025-10-10 18:14:14 +0200

    Update Data.kt

--- app/src/main/java/com/productivity/wind/Data.kt
+++ app/src/main/java/com/productivity/wind/Data.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.lazy.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.productivity.wind.Imports.Data.*
 import kotlin.reflect.*
+import kotlin.reflect.jvm.*
 /*! NEVER move bar and lists to another FOLDER, or other file
 aka....got some functions in datatools, that though a bit tantrum...
 yea....i cant figure out how fix it or rewire it...(kinda lazy--i made it long ago dont remember what did)
