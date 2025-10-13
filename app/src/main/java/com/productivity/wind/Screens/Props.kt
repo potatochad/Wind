33abb46245ee0a19685003ef93a657948cb5ab7a
potatochad <163626408+potatochad@users.noreply.github.com>
2025-10-13 22:06:21 +0200

    Update Props.kt

--- app/src/main/java/com/productivity/wind/Screens/Props.kt
+++ app/src/main/java/com/productivity/wind/Screens/Props.kt
@@ -457,7 +457,7 @@ fun AppSelectPopup(show: m_<Bool>) {
         val myPackage = LocalContext.current.packageName // your app's package
         var appList by r_m(getApps().filter { getAppPackage(it) != myPackage }) // filter self out
 
-        var Loading by r_m(yes)
+        var Loading = r_m(yes)
 
         LazyPopup(
             show = Loading,
@@ -479,8 +479,8 @@ fun AppSelectPopup(show: m_<Bool>) {
             title = "Select App",
             message = "",
             content = {
-                Loading = no
-                Vlog("Loaded: $Loading")
+                Loading.it = no
+                Vlog("Loaded: ${Loading.it}")
 
                 // Only pass the filtered items to the LazyList
                 LazzyList(appList) { app, index ->
