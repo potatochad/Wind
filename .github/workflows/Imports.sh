a9463b394fb23235a7e634c2425dcade515e6628
potatochad <163626408+potatochad@users.noreply.github.com>
2025-10-26 20:13:22 +0900

    Update Imports.sh

--- .github/workflows/Imports.sh
+++ .github/workflows/Imports.sh
@@ -1,26 +1,26 @@
 alias log='echo'
           
-          make_folder() {
-            mkdir -p "$1"
-            log "Folder '$(pwd)/$1' added/exists"
-          }
+make_folder() {
+  mkdir -p "$1"
+  log "Folder '$(pwd)/$1' added/exists"
+}
           
-          try() {
-            CMD=()
-            FAIL=()
-            MODE="cmd"
+try() {
+  CMD=()
+  FAIL=()
+  MODE="cmd"
 
-            for arg in "$@"; do
-            if [[ $arg == "onFail" ]]; then
-            MODE="fail"
-            continue
-            fi
-            if [[ $MODE == "cmd" ]]; then
-              CMD+=("$arg")
-            else
-              FAIL+=("$arg")
-            fi
-            done
-            # Run main command
-            "${CMD[@]}" || "${FAIL[@]}"
-          }
+  for arg in "$@"; do
+  if [[ $arg == "onFail" ]]; then
+  MODE="fail"
+  continue
+  fi
+  if [[ $MODE == "cmd" ]]; then
+  CMD+=("$arg")
+  else
+  FAIL+=("$arg")
+  fi
+  done
+  # Run main command
+  "${CMD[@]}" || "${FAIL[@]}"
+}
