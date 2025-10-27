4dd0e9409c01ca36ece833e5981a18fa9a97b07f
potatochad <163626408+potatochad@users.noreply.github.com>
2025-10-27 20:28:02 +0900

    Update Ignore_Imports.sh

--- app/src/main/java/com/productivity/wind/Imports/Data/Ignore_Imports.sh
+++ app/src/main/java/com/productivity/wind/Imports/Data/Ignore_Imports.sh
@@ -47,3 +47,64 @@ find_and_source() {
   exit 1
   fi
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+Build_APK() {
+
+
+    
+      KEYSTORE_BASE64=${{ secrets.KEYSTORE_BASE64 }}
+      KEYSTORE_PASSWORD=${{ secrets.KEYSTORE_PASSWORD }}
+      KEY_ALIAS=${{ secrets.KEY_ALIAS }}
+      KEY_PASSWORD=${{ secrets.KEY_PASSWORD }}
+
+          set -e
+          mkdir -p app
+          KEYSTORE_FILE="$(pwd)/app/my-release-key.keystore"
+
+          if [ -f "$KEYSTORE_FILE" ]; then
+            echo "✅ Keystore exists locally."
+          elif [ -n "$KEYSTORE_BASE64" ]; then
+            echo "🔹 Restoring keystore from GitHub secret..."
+            echo "$KEYSTORE_BASE64" | tr -d '\n' | base64 --decode > "$KEYSTORE_FILE"
+          else
+            echo "🔹 Creating new keystore..."
+            keytool -genkeypair -v \
+              -keystore "$KEYSTORE_FILE" \
+              -storepass "$KEYSTORE_PASSWORD" \
+              -alias "$KEY_ALIAS" \
+              -keypass "$KEY_PASSWORD" \
+              -keyalg RSA \
+              -keysize 2048 \
+              -validity 10000 \
+              -dname "CN=Temp, OU=Temp, O=Temp, L=Temp, S=Temp, C=US"
+            echo "📦 Base64 of keystore (copy into KEYSTORE_BASE64 secret):"
+            base64 "$KEYSTORE_FILE"
+          fi
+          
+          KEYSTORE_FILE="$(pwd)/app/my-release-key.keystore"
+          ./gradlew assembleRelease -x ktlintCheck -x ktlintKotlinScriptCheck \
+            -Pandroid.injected.signing.store.file="$KEYSTORE_FILE" \
+            -Pandroid.injected.signing.store.password="$KEYSTORE_PASSWORD" \
+            -Pandroid.injected.signing.key.alias="$KEY_ALIAS" \
+            -Pandroid.injected.signing.key.password="$KEY_PASSWORD"
+}
+
+
+
+
+
+
+
+
