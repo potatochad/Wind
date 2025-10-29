50e40fb48b7385cef6df98d5a940e34137936e5e
potatochad <163626408+potatochad@users.noreply.github.com>
2025-10-29 10:37:30 +0900

    Update Banguage.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
@@ -224,7 +224,7 @@ fun <I, O> RememberLauncher(
     return rememberLauncherForActivityResult(contract, onResult)
 }
 
-fun TxtFileToMap(ctx: Context, uri: Uri, fileMap: MutableMap<String, String>) {
+fun TxtFileToMap(ctx: Context, uri: Uri, fileMap: MutableMap<Str, Str>) {
     ctx.contentResolver.openInputStream(uri)?.bufferedReader()?.useLines { lines ->
         lines.forEach { line ->
             if (!line.contains("=")) {
@@ -238,20 +238,8 @@ fun TxtFileToMap(ctx: Context, uri: Uri, fileMap: MutableMap<String, String>) {
 }
 
 
-// Extension to add new items
-fun <T : Any> SnapshotStateList<T>.new(item: T) {
-    this.add(item)
-}
 
-// Edit by index or object reference
-fun <T : Copyable<T>> SnapshotStateList<T>.edit(item: T, block: T.() -> Unit) {
-    val index = indexOf(item)
-    if (index != -1) {
-        // create a modified copy
-        val copy = item.copySelf().apply(block)
-        // assign it back to the same index to trigger recomposition
-        this[index] = copy
-        // tiny trick: touch the list itself to force Compose
-        this[index] = this[index]
-    }
-}
+
+
+
+
