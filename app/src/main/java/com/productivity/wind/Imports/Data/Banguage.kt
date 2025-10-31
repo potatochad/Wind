7431bc4eb2c38a67c3b049980506493f52b26f3f
potatochad <163626408+potatochad@users.noreply.github.com>
2025-10-31 13:24:30 +0900

    Update Banguage.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
@@ -85,7 +85,7 @@ import com.productivity.wind.Imports.*
 import java.util.*
 import com.productivity.wind.R
 import kotlin.reflect.full.*
-
+import androidx.compose.ui.focus.*
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.ActivityResultLauncher
 
