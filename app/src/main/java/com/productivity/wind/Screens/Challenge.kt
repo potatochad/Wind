fdbb0403b238df3dfa12195b9be713fa0134b290
potatochad <163626408+potatochad@users.noreply.github.com>
2025-11-01 19:25:38 +0900

    Update Challenge.kt

--- app/src/main/java/com/productivity/wind/Screens/Challenge.kt
+++ app/src/main/java/com/productivity/wind/Screens/Challenge.kt
@@ -46,12 +46,12 @@ fun CopyPaste() {
         LazyRuleCard("If") {
             LazzyRow {
                 Text("Letter completed: ")
-                LazyInput(letterPoints, maxW = 120)
+                LazyInput(letterPoints, yes, maxW = 120)
                 Text(" points")
             }
             LazzyRow {
                 Text("Text retyped: ")
-                LazyInput(completionPoints, maxW = 120)
+                LazyInput(completionPoints, yes, maxW = 120)
                 Text(" points")
             }
         }
@@ -60,7 +60,7 @@ fun CopyPaste() {
         LazyRuleCard("Other") {
             LazzyRow {
                 Text("Maximum retypes a day: ")
-                LazyInput(Retypes, maxW = 120)
+                LazyInput(Retypes, yes, maxW = 120)
             }
         }
     }
@@ -89,7 +89,7 @@ fun AppUsage() {
           //does nothing YET
           UI.CheckCircle(1, WhichIf)
           Text("Spend ")
-          LazyInput(Time, maxW = 120)
+          LazyInput(Time, yes, maxW = 120, )
           Text(" seconds")
           
           Text(" on ")
@@ -101,7 +101,7 @@ fun AppUsage() {
       LazyRuleCard("Do"){
         LazzyRow{
           Text("Add ")
-          LazyInput(Points, maxW = 120)
+          LazyInput(Points, yes, maxW = 120)
           Text(" points")
         }
       }
