02fdbb40a06fb6f2c87bc0f4b95155c074092b7b
potatochad <163626408+potatochad@users.noreply.github.com>
2025-11-02 18:42:23 +0900

    Update ImportsUI.kt

--- app/src/main/java/com/productivity/wind/Imports/ImportsUI.kt
+++ app/src/main/java/com/productivity/wind/Imports/ImportsUI.kt
@@ -93,7 +93,9 @@ fun Vlog(msg: Str, special: Str = "none", delayLevel: Int = 0) {
 }
 
 fun getMyAppLogs(): Str {
-    val process = Runtime.getRuntime().exec("logcat -d *:V")
+	val process = Runtime.getRuntime().exec("logcat -d | grep ${App.ctx.packageName}")
+
+    // val process = Runtime.getRuntime().exec("logcat -d *:V")
     val reader = BufferedReader(InputStreamReader(process.inputStream))
     val logs = mutableListOf<Str>()
     val myPackage = App.ctx.packageName
