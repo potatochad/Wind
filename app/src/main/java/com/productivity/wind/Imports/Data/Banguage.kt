d6dd6bc3e97a06b26478410914bf4f95301a919e
potatochad <163626408+potatochad@users.noreply.github.com>
2025-11-04 13:00:41 +0100

    Update Banguage.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
@@ -177,6 +177,15 @@ fun move(s: Any = 0, w: Any = 0, h: Any = 0) {
 		}
 	)
 }
+fun Modifier.move(s: Any = 0, h: Any = s, w: Any = s): Modifier =
+    this.then(
+        Modifier.offset(
+            x = toDp(w), 
+            y = toDp(h)
+        )
+    )
+
+
 
 fun Dp.toPx(): Int {
     var context = App.ctx
