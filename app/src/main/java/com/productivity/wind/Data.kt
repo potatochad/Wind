4550c1f4124a9c91054d5ad1779d4d22258e84f9
potatochad <163626408+potatochad@users.noreply.github.com>
2025-11-12 21:37:26 +0100

    Update Data.kt

--- app/src/main/java/com/productivity/wind/Data.kt
+++ app/src/main/java/com/productivity/wind/Data.kt
@@ -143,7 +143,8 @@ data class AppTsk(
     var pkg: Str = "",
     var NowTime: Int = 0,
     var DoneTime: Int = 0,
-    var Worth: m_<Int> = m(0),
+    // var Worth: Int by m(0),
+	var Worth: Int = 0,
 )
 
 @Serializable
