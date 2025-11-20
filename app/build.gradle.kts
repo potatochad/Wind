104b6b9ca2f975492f7ede813d4ce8976439a391
potatochad <163626408+potatochad@users.noreply.github.com>
2025-11-20 21:07:08 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,6 +93,8 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "kotlin/reflect/reflect.kotlin_builtins"
+   
         }
     }
 }
