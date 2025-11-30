5bd8bfe490f10e173571844ff809f5b4bb5da3cc
potatochad <163626408+potatochad@users.noreply.github.com>
2025-11-30 17:40:49 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,6 +100,18 @@ excludes += setOf(
 
         }
     }
+
+
+
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.jetbrains.kotlin") {
+            useVersion("1.9.0")
+        }
+    }
+}
+
+
 }
 
 ktlint {
