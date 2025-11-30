65a7b48c35a79e016c008b03d38d24821442e845
potatochad <163626408+potatochad@users.noreply.github.com>
2025-11-30 09:06:13 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,11 +106,10 @@ ktlint {
 
 
 dependencies {
-implementation("org.jetbrains.kotlin:kotlin-scripting-common:1.9.23")
-implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:1.9.23")
-implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies:1.9.23")
-implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven:1.9.23")
-
+implementation("org.jetbrains.kotlin:kotlin-scripting-common:2.0.10")
+implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:2.0.10")
+implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies:2.0.10")
+implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven:2.0.10")
 
 
     implementation("androidx.compose.foundation:foundation:1.9.2")
