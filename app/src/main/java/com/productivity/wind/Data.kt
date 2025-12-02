d9c525235fcfc3b2b79ac3a727108844a109f56b
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-02 21:54:30 +0100

    Update Data.kt

--- app/src/main/java/com/productivity/wind/Data.kt
+++ app/src/main/java/com/productivity/wind/Data.kt
@@ -37,42 +37,10 @@ fun RuntimeKotlin(){
 		code: Str = """fun runMod() { Vlog("MOD $modName JUST RAN") }""",
 	) {
 		val modsFolder = newFolder("Mods")
-		val modFile = File(modsFolder, "$modName.kt")
+		val modFile = File(modsFolder, "Mod.kt")
 
 		val fullCode = """
 		package com.productivity.wind.Imports.Mods
-
-        import com.productivity.wind.Screens.*
-		import android.content.Context
-		import android.os.Build
-		import androidx.annotation.RequiresApi
-		import androidx.navigation.NavHostController
-		import kotlinx.coroutines.delay
-		import android.os.Bundle
-		import androidx.activity.ComponentActivity
-		import androidx.activity.compose.setContent
-		import androidx.compose.foundation.gestures.detectTapGestures
-		import androidx.compose.foundation.layout.*
-		import androidx.compose.material3.*
-		import androidx.compose.runtime.*
-		import androidx.compose.ui.*
-		import androidx.compose.ui.unit.dp
-		import androidx.navigation.compose.rememberNavController
-		import androidx.compose.ui.platform.LocalConfiguration
-		import com.productivity.wind.Imports.*
-		import androidx.core.view.*
-		import androidx.compose.foundation.lazy.*
-		import androidx.compose.runtime.snapshots.SnapshotStateList
-		import com.productivity.wind.Imports.Data.*
-		import kotlin.reflect.*
-		import kotlin.reflect.jvm.*
-		import kotlinx.serialization.*
-		import kotlinx.serialization.json.*
-		import androidx.compose.foundation.text.selection.*
-		import androidx.compose.ui.input.pointer.pointerInput
-		import kotlin.system.*
-		import dalvik.system.*
-		import java.io.File
 
 		class ModClass {
      	    $code
