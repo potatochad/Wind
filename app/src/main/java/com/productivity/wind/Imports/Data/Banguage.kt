6c722b753ddaf4612a961f9751069ef636180466
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-06 23:04:12 +0100

    Update Banguage.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
@@ -177,9 +177,26 @@ fun m_<Web?>.txt(done: DoStr = {}) {
     }
 }
 
+object Popup {
+    private val popups = mutableListOf<
+        kotlin.Pair<m_<Bool>, ui_<m_<Bool>>>
+    >()
 
+    fun add(popup: ui_<m_<Bool>>): m_<Bool> {
+        val state = m(no)
+        popups.add(kotlin.Pair(state, popup))
+        return state
+    }
 
-
+    @Composable
+    fun Init() {
+        for (pair in popups) {
+            val state = pair.first
+            val block = pair.second
+            block(state)
+        }
+    }
+}
 
 
 
