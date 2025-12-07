1b073bfd8638c3e5aafdbdae90a5b7597085101a
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-07 18:11:09 +0100

    Update Renames.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/Renames.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/Renames.kt
@@ -1,1 +1,8 @@
+fun Mod.w(min: Any?, max: Any? = min) = this.widthIn(max = toDp(max), min = toDp(min))
+fun Mod.h(min: Any?, max: Any? = min) = this.heightIn(max = toDp(max), min = toDp(min))
+fun Mod.s(value: Any?) = this.size(toDp(value))
+
+fun Modifier.maxS(): Mod= this.fillMaxSize()
+fun Mod.maxW(): Mod= this.fillMaxWidth()
+fun Mod.maxH(): Mod= this.fillMaxHeight()
 
