203d1dbe27cfe7e5cc023daf242be79d5a05f826
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-07 18:10:58 +0100

    Update Banguage.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
@@ -318,14 +318,6 @@ fun Mod.clickOrHold(
 	}
 }
 
-fun Mod.w(min: Any?, max: Any? = min) = this.widthIn(max = toDp(max), min = toDp(min))
-fun Mod.h(min: Any?, max: Any? = min) = this.heightIn(max = toDp(max), min = toDp(min))
-fun Mod.s(value: Any?) = this.size(toDp(value))
-
-fun Modifier.maxS(): Mod= this.fillMaxSize()
-fun Mod.maxW(): Mod= this.fillMaxWidth()
-fun Mod.maxH(): Mod= this.fillMaxHeight()
-
 @Composable
 fun move(s: Any = 0, w: Any = 0, h: Any = 0) {
 	val sDp = toDp(s)
