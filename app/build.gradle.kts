ab901257139a9f165a22fc669172b498221dfcc9
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-09 22:07:34 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,9 +109,11 @@ ktlint {
 
 
 dependencies {
+    implementation("com.google.maps.android:maps-compose:6.9.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0") 
+
+
     implementation("com.google.android.gms:play-services-location:21.3.0")
-
-
     implementation("org.mozilla:rhino:1.7.15")
     compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.1.10")
     implementation("androidx.compose.foundation:foundation:1.9.2")
