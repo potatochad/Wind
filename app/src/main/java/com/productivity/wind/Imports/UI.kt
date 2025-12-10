af2246962e5ca420baeb2846e0dcb3eb0b8712a5
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-10 12:42:56 +0100

    Update UI.kt

--- app/src/main/java/com/productivity/wind/Imports/UI.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI.kt
@@ -182,26 +182,7 @@ fun correctStr(target: Str, input: Str): AnnotatedString {
     }
 }
 
-fun CopyTskInput(tsk: CopyTsk, newInput: Str, Done: Do) {
-	log("1) tsk.input: [${tsk.input}], newInput: [$newInput]")
-    Bar.copyTsk.edit(tsk) { tsk.input = newInput }
 
-	log("2) tsk.input: [${tsk.input}], newInput: [$newInput]")
- 
-
-    val goodStr = CopyTskCorrectInput(tsk).size
-    val delta = goodStr - tsk.goodStr
-    if (delta > 0) {
-        Bar.funTime += delta * tsk.Letter_Worth
-        Bar.copyTsk.edit(tsk) { tsk.goodStr = goodStr }
-    }
-
-	log("3) tsk.input: [${tsk.input}], newInput: [$newInput]")
- 
-
-    if (newInput.size > tsk.input.size) Bar.LettersTyped++
-    if (CopyTskCorrectInput(tsk) == tsk.txt) Done()
-}
 fun CopyTskSimpleAutoCorrect(tsk: CopyTsk) {
     val input5 = tsk.input.takeLast(5)
     val good5 = tsk.txt.fromTo(tsk.goodStr, tsk.goodStr + 5)
