877450eb940be01154fa441c9b63dcc6425ef916
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-13 18:37:00 +0100

    Update web.kt

--- app/src/main/java/com/productivity/wind/Screens/web.kt
+++ app/src/main/java/com/productivity/wind/Screens/web.kt
@@ -32,7 +32,8 @@ fun Web(){
 
     RunOnce {
         Bar.Url = "${web.url}"
-        web.url("https://player.fm/series/my-first-million")
+        web.url("https://google.com")
+        // web.url("https://player.fm/series/my-first-million")
     }
 
 
