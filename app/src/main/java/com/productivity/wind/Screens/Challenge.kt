9d5ff4574200a3465a041f6bc9894ef531d51fc3
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-15 15:27:31 +0100

    Update Challenge.kt

--- app/src/main/java/com/productivity/wind/Screens/Challenge.kt
+++ app/src/main/java/com/productivity/wind/Screens/Challenge.kt
@@ -137,6 +137,15 @@ fun CopyTskUI(tsk: CopyTsk) {
 
     RunOnce(tsk.goodStr) {
 		if (tsk.goodStr > 20) {
+			val done = toF(txtScroll.maxValue) / toF(tsk.txt.size)
+			Vlog("done: $done")
+
+			txtScroll.scroll(done)
+		}
+	}
+	RunOnce {
+		Vlog("Scroll to progress")
+		if (tsk.goodStr > 20) {
 			val done = toF(tsk.goodStr) / toF(tsk.txt.size)
 
 			scrollToProgress(done, txtScroll)
