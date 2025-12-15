fa2fe1e00f8c2d99ade944e1fee0ad6dac99df1e
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-15 07:51:44 +0100

    Update Challenge.kt

--- app/src/main/java/com/productivity/wind/Screens/Challenge.kt
+++ app/src/main/java/com/productivity/wind/Screens/Challenge.kt
@@ -183,7 +183,7 @@ fun CopyTskUI(tsk: CopyTsk) {
     move(h = 20)
 
 	var txt = r_m(tsk.input)
-    Item.BigTskInput(txt) {
+    OutlinedInput(txt) {
 		if (it.size - txt.it.size < 2) {
 			txt.it=it
 			
