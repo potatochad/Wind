57e38b295eec19946946fb2bc6d4c57841a10384
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-17 17:36:38 +0100

    Update Banguage.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
@@ -124,23 +124,6 @@ import android.content.*
 import android.net.*
 
 
-fun Web?.url(url: Str) {
-    this?.loadUrl(url)
-}
-fun Web?.reload() {
-	this?.reload()
-}
-fun m_<Web?>.reload() {
-    this.it?.reload()
-}
-fun m_<Web?>.url(url: Str) {
-    this.it?.loadUrl(url)
-}
-val Web?.url: Str
-    get() = this?.url ?: ""
-
-val m_<Web?>.url: Str
-    get() = this.it?.url ?: ""
 
 fun goBackWeb(web: Web?) {
     web?.post {
