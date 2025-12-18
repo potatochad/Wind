b014943e4eea6cdd923acb4f3738a0b1d659920b
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-18 20:58:16 +0200

    Update UI.kt

--- app/src/main/java/com/productivity/wind/Imports/UI.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI.kt
@@ -386,7 +386,7 @@ object UI {
         bottomPadding: Dp = 20.dp,
         StartPaddingRemove: Int = 40,
     ) {
-        val safeStartPadding = max(0.dp, (App.w+60.dp) / 4 - StartPaddingRemove.dp)
+        val safeStartPadding = max(0.dp, (AppW+60.dp) / 4 - StartPaddingRemove.dp)
 
         Column(
             Modifier.space(start = safeStartPadding),
