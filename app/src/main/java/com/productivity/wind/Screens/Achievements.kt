603f158f7a5ee5a750a2b5ad6d21a240255615dd
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-19 17:27:24 +0200

    Update Achievements.kt

--- app/src/main/java/com/productivity/wind/Screens/Achievements.kt
+++ app/src/main/java/com/productivity/wind/Screens/Achievements.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.*
 import androidx.compose.material.icons.Icons
 import com.productivity.wind.Imports.*
 import androidx.compose.material.icons.outlined.*
-import com.productivity.wind.Imports.Data.Bar
+import com.productivity.wind.Imports.Data.*
 
 
 @Composable
