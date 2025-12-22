c0d926c37d161d1b1274aeec370e1e8dbfef1e97
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-22 12:37:42 +0200

    Update UItext.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/UItext.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/UItext.kt
@@ -138,28 +138,27 @@ fun UIText(text: Any, style: StrStyle = StrStyle()): UIStr {
     }
 }
 
-fun UIStr.getStyle(): StrStyle {
-    return this.spanStyles.firstOrNull()?.item ?: StrStyle()
-}
+fun Any.getStyle(): StrStyle { return toUIStr(this).spanStyles.firstOrNull()?.item ?: StrStyle() }
 
-fun Any.size(value: TextUnit): UIStr {
-    val uiStr = toUIStr(this)
-    return UIText(uiStr, uiStr.getStyle().copy(fontSize = value))
-}
+fun Any.size(x: TextUnit): UIStr { return UIText(this, this.getStyle().copy(fontSize = x)) }
 
-fun Any.bold(): UIStr {
-	val uiStr = toUIStr(this)
-	return UIText(uiStr, uiStr.getStyle().copy(fontWeight = FontWeight.Bold))
-}
+fun Any.bold(): UIStr { return UIText(this, this.getStyle().copy(fontWeight = FontWeight.Bold)) }
 
-fun Any.color(value: Color): UIStr {
-	val uiStr = toUIStr(this)
-	return UIText(uiStr, uiStr.getStyle().copy(color = value))
-}
-fun Any.gold(): UIStr {
-	val uiStr = toUIStr(this)
-	return UIText(uiStr, uiStr.getStyle().copy(color = Gold))
-}
+fun Any.color(x: Color): UIStr { return UIText(this, this.getStyle().copy(color = x)) }
+
+fun Any.gold(): UIStr { return UIText(this, this.getStyle().copy(color = Gold)) }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
