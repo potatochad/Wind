f75e61a80829b56e4ab905911acf29834b61ee1d
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-22 12:03:35 +0200

    Update UItext.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/UItext.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/UItext.kt
@@ -1,1 +1,43 @@
 
+
+
+
+
+
+
+fun UIText(text: UIStr, style: StrStyle = StrStyle()): UIStr {
+    return makeUIStr {
+        pushStyle(style)
+        add(text)
+        pop()
+    }
+}
+
+fun UIStr.getStyle(): StrStyle {
+    return this.spanStyles.firstOrNull()?.item ?: StrStyle()
+}
+
+fun Any.size(value: TextUnit): UIStr {
+    val uiStr = toUIStr(this)
+    return UIText(uiStr, uiStr.getStyle().copy(fontSize = value))
+}
+
+fun Any.bold(): UIStr {
+	val uiStr = toUIStr(this)
+	return UIText(uiStr, uiStr.getStyle().copy(fontWeight = FontWeight.Bold))
+}
+
+fun Any.color(value: Color): UIStr {
+	val uiStr = toUIStr(this)
+	return UIText(uiStr, uiStr.getStyle().copy(color = value))
+}
+fun Any.gold(): UIStr {
+	val uiStr = toUIStr(this)
+	return UIText(uiStr, uiStr.getStyle().copy(color = Gold))
+}
+
+
+
+
+
+
