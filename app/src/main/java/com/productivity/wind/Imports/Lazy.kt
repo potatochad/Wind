d3efaf3beea41bc21202fe109d352ba88519022a
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-26 17:24:35 +0200

    Update Lazy.kt

--- app/src/main/java/com/productivity/wind/Imports/Lazy.kt
+++ app/src/main/java/com/productivity/wind/Imports/Lazy.kt
@@ -805,7 +805,6 @@ fun LazyPopup(
     show: m_<Bool>,
     title: Str = "Info",
 	msg: Str = "",
-	mod: Mod = Mod,
     showCancel: Bool = yes,
     showConfirm: Bool = yes,
     onConfirm: Do? = null,
@@ -828,7 +827,6 @@ fun LazyPopup(
 				ui?.invoke() ?: Text(msg)
 			}
         },
-		modifier = mod,
         confirmButton = {
             if (showConfirm) {
 				move(15)
