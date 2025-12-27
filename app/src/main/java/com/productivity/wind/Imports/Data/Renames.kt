02ae119998379df07ba31a47dca2f456911d5b15
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-27 23:16:53 +0200

    Update Renames.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/Renames.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/Renames.kt
@@ -140,7 +140,7 @@ var Scroll.it: Int
 fun Mod.w(min: Any?, max: Any? = min) = this.widthIn(max = toDp(max), min = toDp(min))
 fun Mod.h(min: Any?, max: Any? = min) = this.heightIn(max = toDp(max), min = toDp(min))
 fun Mod.s(value: Any?) = this.size(toDp(value))
-fun Mod.center() = this.wrapContent().align(Alignment.Center)
+fun Mod.center() = this.wrapContentSize(align = Alignment.Center)
 
 fun Mod.maxS(): Mod= this.fillMaxSize()
 fun Mod.maxW(): Mod= this.fillMaxWidth()
