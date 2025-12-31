2e5397001ec5f55e9eb3d065d2a0d5748801ad4c
potatochad <163626408+potatochad@users.noreply.github.com>
2025-12-31 19:54:43 +0200

    Update Banguage.kt

--- app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
+++ app/src/main/java/com/productivity/wind/Imports/Data/Banguage.kt
@@ -599,7 +599,7 @@ fun getMyAppLogs() {
 			val s = line.replace(Regex("""^\d{2}-\d{2}\s+|\s+\d+\s+\d+\s+"""), " ").takeLast(3000)
 			if ("ApkAssets: Deleting" in s) return@forEachLine
 			if ("WindowOnBackDispatcher" in s) return@forEachLine
-
+			if (" W " in s) return@forEachLine
 
 			val last = Bar.logs.lastOrNull()
 
