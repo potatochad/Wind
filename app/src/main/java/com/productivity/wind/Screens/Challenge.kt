0d4c28a740b00db7320755bc8c1565f25c3177ae
potatochad <163626408+potatochad@users.noreply.github.com>
2026-01-03 11:15:32 +0200

    Update Challenge.kt

--- app/src/main/java/com/productivity/wind/Screens/Challenge.kt
+++ app/src/main/java/com/productivity/wind/Screens/Challenge.kt
@@ -133,6 +133,7 @@ fun CopyPaste(id: Str ="") {
 @Composable
 fun CopyTskUI(tsk: CopyTsk) {
     val txtScroll = r_Scroll()
+	var bigText by r_m(UISTR)
 	val inputScroll = r_Scroll()
 	var scrollBy by r_m(toF(AppW)/180f)
 	var done by r_m(no)
@@ -185,7 +186,7 @@ fun CopyTskUI(tsk: CopyTsk) {
     }
     move(8)
 	Text(
-		goodStrCopyTsk(tsk),
+		,
 		modifier = Mod.space(h = 15).h(100).maxW().Vscroll(txtScroll)
 	)
     move(h = 20)
