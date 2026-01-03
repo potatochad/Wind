baceeccbf920927443fbbfca806850a08e0e1a1b
potatochad <163626408+potatochad@users.noreply.github.com>
2026-01-03 12:41:34 +0200

    Update Challenge.kt

--- app/src/main/java/com/productivity/wind/Screens/Challenge.kt
+++ app/src/main/java/com/productivity/wind/Screens/Challenge.kt
@@ -144,7 +144,7 @@ fun CopyTskUI(tsk: CopyTsk) {
 			log("scrollBy: $scrollBy, ${tsk.goodStr}")
 		}
 		RunOnce {
-			Bar.copyTsk.edit(tsk) { tsk.goodStr = CopyTskgoodStr(tsk) }
+			Bar.copyTsk.edit(tsk) { tsk.goodStr = tsk.goodStr() }
 			wait {
 				if (tsk.goodStr > 30) {
 					val done = toF(tsk.goodStr)*(scrollBy)
