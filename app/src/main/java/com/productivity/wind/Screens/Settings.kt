668414be9bf9e0648b7808f2ff5e9b22e551271e
potatochad <163626408+potatochad@users.noreply.github.com>
2026-01-03 15:13:21 +0200

    Update Settings.kt

--- app/src/main/java/com/productivity/wind/Screens/Settings.kt
+++ app/src/main/java/com/productivity/wind/Screens/Settings.kt
@@ -47,15 +47,11 @@ fun SettingsScreen() {
             BigIconColor = DarkBlue,
             title = "BackUp",
 			endContent = {
-				Row(
-					horizontalArrangement = Arrangement.Center,
-				{
 					LazySwitch(Bar.encryptedBackup){
 						Bar.encryptedBackup = it
 						Vlog("encrypt data: [Bar.encryptedBackup]")
 					}
 					move(12)
-				}
 			},
             onClick = { 
                 backup.it = yes
