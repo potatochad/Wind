cf00d464f1eaf305fb80eb506b215492d8f25a8a
potatochad <163626408+potatochad@users.noreply.github.com>
2026-01-21 22:03:04 +0100

    Update Input.kt

--- app/src/main/java/com/productivity/wind/Imports/UI_visible/Input.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI_visible/Input.kt
@@ -73,6 +73,21 @@ import com.productivity.wind.Imports.*
 import com.productivity.wind.Imports.Data.*
 
 
+val KwikColorPrimary = Color(0xFF2196F3)
+val KwikColorSecondary = Color(0x9A2196F3)
+val KwikColorTertiary = Color(0xFFFF5722)
+val KwikColorWarning = Color(0xFFFF9800)
+val KwikColorYellow = Color(0xFFFFEB3B)
+val KwikColorError = Color(0xFFF44336)
+val KwikColorSuccess = Color(0xFF4CAF50)
+val KwikColorHint = Color(0xFF9B9B9B)
+val KwikColorFilledTextFieldFocused = Color(0xFFE1E1E1)
+val KwikColorFilledTextFieldUnfocused = Color(0xFFEAEAEA)
+val KwikColorFilledTextFieldFocusedDarkMode = Color(0xFF424242)
+val KwikColorFilledTextFieldUnfocusedDarkMode = Color(0x19ECEBEB)
+val KwikColorFilledTextFieldError = Color(0x34F44336)
+
+
 /**
  * A versatile filled text field component that can be used to take user input.
  * @param modifier: The modifier for the text field.
