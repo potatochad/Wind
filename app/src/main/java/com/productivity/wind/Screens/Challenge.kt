500ba4288eb32b2f30841cc979d988f3aa59aac4
potatochad <163626408+potatochad@users.noreply.github.com>
2026-02-01 20:29:01 +0100

    Update Challenge.kt

--- app/src/main/java/com/productivity/wind/Screens/Challenge.kt
+++ app/src/main/java/com/productivity/wind/Screens/Challenge.kt
@@ -481,10 +481,11 @@ fun DoTskUI(tsk: DoTsk) = LazzyRow {
 		log("name ${tsk.name}, tskOn $tskOn, !it ${!it}")
 	}
 	move(5)
-    Text(
-		"${tsk.name}: ${Time(tsk.timeLeft)}",
-		modifier = Modifier.weight(1f), 
-	)
+	Row(Mod.weight(1f)) {
+		Text(
+			"${tsk.name}: ${Time(tsk.timeLeft)}",
+		)
+	}
     End { 
 		Item.Edit{
             Item.enoughPoints {
