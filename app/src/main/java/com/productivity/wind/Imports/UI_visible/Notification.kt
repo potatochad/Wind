54b4fa3903053d246656cc57e59141ab30efcd29
potatochad <163626408+potatochad@users.noreply.github.com>
2026-02-09 10:55:31 +0200

    Update Notification.kt

--- app/src/main/java/com/productivity/wind/Imports/UI_visible/Notification.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI_visible/Notification.kt
@@ -1,6 +1,6 @@
 package com.productivity.wind.Imports.UI_visible
  
-import android.annotation.SuppressLint
+import android.annotation.*
 import androidx.compose.ui.draw.*
 import androidx.compose.foundation.shape.*
 import androidx.compose.material3.*
@@ -62,20 +62,27 @@ import android.widget.*
 import android.app.*
 import android.content.*
 import android.graphics.*
-
-
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.core.app.NotificationCompat
 import androidx.media.session.*
+import android.app.*
+import android.content.*
+import android.provider.*
 
 
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.provider.Settings
-import androidx.annotation.RequiresApi
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.res.*
+import androidx.lifecycle.*
+import androidx.lifecycle.compose.*
+import com.google.accompanist.permissions.*
+import android.app.NotificationManager.*
+import androidx.core.app.NotificationCompat.*
+import androidx.core.app.*
+import androidx.core.graphics.drawable.*
+import java.util.logging.*
+
+
 
 @RequiresApi(Build.VERSION_CODES.S)
 fun startSystemTimer(context: Context, minutes: Int) {
@@ -312,61 +319,8 @@ fun LiveUpdateNotification(
 
 
 
-/*
- * Copyright 2025 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 
-import android.annotation.SuppressLint
-import android.app.NotificationManager
-import android.content.Context
-import android.content.Intent
-import android.os.Build
-import android.provider.Settings
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LifecycleEventEffect
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
-import kotlinx.coroutines.launch
 
 @RequiresApi(Build.VERSION_CODES.BAKLAVA)
 @Composable
@@ -491,38 +445,6 @@ private fun NotificationPermissionCard(
     }
 }
 
-/*
- * Copyright 2025 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.example.platform.ui.live_updates
-
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.NotificationManager.IMPORTANCE_DEFAULT
-import android.content.Context
-import androidx.core.app.NotificationCompat.ProgressStyle
-import android.graphics.Color
-import android.os.Build
-import android.os.Handler
-import android.os.Looper
-import androidx.annotation.RequiresApi
-import androidx.core.app.NotificationCompat
-import androidx.core.graphics.drawable.IconCompat
-import java.util.logging.Level
-import java.util.logging.Logger
 
 object SnackbarNotificationManager {
     private lateinit var notificationManager: NotificationManager
