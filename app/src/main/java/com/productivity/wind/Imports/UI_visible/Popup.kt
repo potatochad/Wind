56f94389fd2f887bf1e7ade7ec1f0c843e5de500
potatochad <163626408+potatochad@users.noreply.github.com>
2026-02-13 18:42:56 +0200

    Update Popup.kt

--- app/src/main/java/com/productivity/wind/Imports/UI_visible/Popup.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI_visible/Popup.kt
@@ -93,25 +93,7 @@ fun AllowAppUsage() {
 
 
 
-@Composable
-fun BtnFloating(
-	Do: Do,
-) {
-    Popup(
-        alignment = Alignment.BottomEnd,
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            contentAlignment = Alignment.BottomEnd
-        ) {
-            Icon.Add {
-				Vlog("clicked btn")
-			}
-        }
-    }
-}
+
 
 
 
