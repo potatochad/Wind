edbeb2e2899f39a96dd18a8d5bfca54217813327
potatochad <163626408+potatochad@users.noreply.github.com>
2026-02-26 07:53:00 +0100

    Update Browser.kt

--- app/src/main/java/com/productivity/wind/Imports/UI_visible/Browser.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI_visible/Browser.kt
@@ -1,4 +1,81 @@
-package com.isakaro.kwik.ui.webview
+package com.productivity.wind.Imports.UI_visible
+
+import com.productivity.wind.Imports.Utils.*
+import android.app.usage.UsageStatsManager
+import android.app.*
+import androidx.core.app.*
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.unit.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import android.os.*
+import android.content.*
+import android.util.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.text.font.*
+import kotlinx.coroutines.*
+import android.net.Uri
+import android.widget.Toast
+import com.productivity.wind.*
+import kotlin.collections.*
+import android.content.ClipData
+import android.content.ClipboardManager
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.res.painterResource
+import android.content.Intent
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.annotation.RequiresApi
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.productivity.wind.Screens.*
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.text.rememberTextMeasurer
+import android.graphics.drawable.Drawable
+import android.content.pm.*
+import java.util.*
+import com.productivity.wind.R
+import com.productivity.wind.Imports.UI_visible.*
+import androidx.compose.ui.geometry.*
+import androidx.compose.ui.graphics.drawscope.*
+import java.time.*
+import kotlin.concurrent.schedule
+import java.io.*
+import androidx.compose.ui.text.input.*
+import androidx.compose.ui.text.*
+import android.text.*
+import android.text.style.*
+import androidx.compose.ui.viewinterop.*
+import android.widget.*
+import android.text.method.*
+import androidx.compose.ui.unit.*
+import com.productivity.wind.Imports.*
+import androidx.compose.ui.window.*
+import androidx.compose.animation.*
+
+
+
 
 import android.annotation.SuppressLint
 import android.content.Intent
@@ -30,7 +107,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.net.toUri
-import com.isakaro.kwik.ui.loading.KwikCircularLoading
+
+
+@Composable
+fun KwikCircularLoading() {
+    CircularProgressIndicator()
+}
 
 /**
  * Settings for the KwikWebView.
