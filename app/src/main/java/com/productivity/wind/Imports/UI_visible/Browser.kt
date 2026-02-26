e2c9be8a80bb1f80545f54c3343baa23742c7cb0
potatochad <163626408+potatochad@users.noreply.github.com>
2026-02-26 21:20:11 +0100

    Update Browser.kt

--- app/src/main/java/com/productivity/wind/Imports/UI_visible/Browser.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI_visible/Browser.kt
@@ -73,13 +73,6 @@ import androidx.compose.ui.unit.*
 import com.productivity.wind.Imports.*
 import androidx.compose.ui.window.*
 import androidx.compose.animation.*
-
-
-
-
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.net.Uri
 import android.view.ViewGroup
 import android.webkit.CookieManager
 import android.webkit.ValueCallback
@@ -95,14 +88,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
