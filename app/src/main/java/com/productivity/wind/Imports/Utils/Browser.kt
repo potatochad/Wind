1f9582a7371c51762b2e5234a008c576e5afa370
potatochad <163626408+potatochad@users.noreply.github.com>
2026-03-01 20:21:29 +0100

    Update Browser.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/Browser.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/Browser.kt
@@ -1,1 +1,83 @@
+package com.productivity.wind.Imports
 
+import android.annotation.SuppressLint
+import android.content.*
+import androidx.compose.runtime.*
+import androidx.activity.compose.*
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.compose.ui.viewinterop.*
+import android.view.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import com.productivity.wind.*
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.productivity.wind.R
+import com.productivity.wind.Imports.*
+import android.webkit.*
+import android.graphics.*
+import kotlinx.coroutines.*
+import com.productivity.wind.Imports.Utils.*
+
+
+
+@SuppressLint("SetJavaScriptEnabled")
+fun WebView.clearWebData() {
+    clearMatches()
+    clearHistory()
+    clearFormData()
+    clearSslPreferences()
+    clearCache(yes)
+
+    CookieManager.getInstance().removeAllCookies(null)
+    WebStorage.getInstance().deleteAllData()
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+fun WebSettings.applyDefaultConfig() {
+    javaScriptEnabled = yes
+    setSupportZoom(yes)
+    builtInZoomControls = yes
+    displayZoomControls = no
+}
+
+@Composable
+fun UrlLong(input: Str): Str {
+    return remember(input) {
+        if (input.startsWith("http://") || input.startsWith("https://")) {
+            input
+        } else {
+            "https://$input"
+        }
+    }
+}
+@Composable
+fun UrlShort(x: Str): Str {
+    return remember(input) {
+        x.remove("https://").remove("http://").remove("www.")
+    }
+}
+
+fun BlockKeywords(
+    webView: m_<Web?>,
+    keywords: List<Str>,
+    Do: Do = {},
+) {
+    webView.it?.evaluateJavascript(
+        "(function() { return document.body.innerText.toLowerCase(); })();"
+    ) { html ->
+        if (html != null) {
+            val lowerHtml = html.lowercase()
+            for (word in keywords) {
+                if (lowerHtml.contains(word.lowercase())) {
+                    goBackWeb(webView.it)
+                    Do()
+                    Vlog("Blocked $word") 
+                    return@evaluateJavascript
+                }
+            }
+        }
+    }
+}
