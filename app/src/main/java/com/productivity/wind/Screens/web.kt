08064a70847a70568cdccde0ec75b5f2132fba69
potatochad <163626408+potatochad@users.noreply.github.com>
2026-03-03 21:39:20 +0100

    Update web.kt

--- app/src/main/java/com/productivity/wind/Screens/web.kt
+++ app/src/main/java/com/productivity/wind/Screens/web.kt
@@ -44,7 +44,7 @@ fun Web(){
         Bar.Url = url ?: "https://www.google.com"
 
         web.html { x ->
-            log("full html: [$x]", 5000)
+            log("full html: [$x]", 3000)
         }
 
         Bar.badWords.each { y->
