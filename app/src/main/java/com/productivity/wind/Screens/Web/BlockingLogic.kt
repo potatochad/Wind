ea584387f02a3a13a9e173c0345412facf56209c
potatochad <163626408+potatochad@users.noreply.github.com>
2026-03-09 20:31:14 +0100

    Update BlockingLogic.kt

--- app/src/main/java/com/productivity/wind/Screens/Web/BlockingLogic.kt
+++ app/src/main/java/com/productivity/wind/Screens/Web/BlockingLogic.kt
@@ -1,1 +1,35 @@
+package com.productivity.wind.Screens.Web
+
+import androidx.compose.runtime.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import com.productivity.wind.*
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.productivity.wind.R
+import com.productivity.wind.Imports.*
+import android.annotation.SuppressLint
+import android.view.*
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.graphics.Bitmap
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.activity.compose.*
+import com.productivity.wind.Imports.Utils.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.unit.*
+import androidx.compose.foundation.shape.*
+import androidx.compose.ui.text.style.*
+import androidx.compose.ui.graphics.*
+import com.productivity.wind.Imports.UI_visible.*
+import androidx.compose.ui.platform.*
+import androidx.compose.material3.pulltorefresh.*
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
 
