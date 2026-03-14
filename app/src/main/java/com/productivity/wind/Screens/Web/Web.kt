c5856b361920553e306cd45db5439c7282856147
potatochad <163626408+potatochad@users.noreply.github.com>
2026-03-14 17:14:28 +0100

    Update Web.kt

--- app/src/main/java/com/productivity/wind/Screens/Web/Web.kt
+++ app/src/main/java/com/productivity/wind/Screens/Web/Web.kt
@@ -43,7 +43,7 @@ fun WebHome(){
         top = {
             Text("Home")
 
-			TinyInput(Bar.Url, Mod.h(40).w(AppW - 150.dp), isInt = no, maxLetters = 400){       
+			TinyInput(Bar.Url, Mod.h(40).w(AppW - 180.dp), isInt = no, maxLetters = 400){       
 				Bar.Url = it
 			}
     
@@ -80,7 +80,7 @@ fun Web(){
     LazyScreen(
         top = {
             
-			TinyInput(Bar.Url, Mod.h(40).w(AppW - 150.dp), isInt = no, maxLetters = 400){       
+			TinyInput(Bar.Url, Mod.h(40).w(AppW - 180.dp), isInt = no, maxLetters = 400){       
 				Bar.Url = it
 			}
 				
