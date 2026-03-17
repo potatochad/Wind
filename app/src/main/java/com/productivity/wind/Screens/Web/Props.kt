aa1d359128cd9d2fcb5536654571bcf06ab5770c
potatochad <163626408+potatochad@users.noreply.github.com>
2026-03-17 13:02:25 +0100

    Update Props.kt

--- app/src/main/java/com/productivity/wind/Screens/Web/Props.kt
+++ app/src/main/java/com/productivity/wind/Screens/Web/Props.kt
@@ -44,7 +44,7 @@ object WebProps {
 			LazyCard(
 				modUI = Mod.space(start = 8),
 				modCard = Mod.space(h=8, w=10).maxW().click {    
-					goTo("WebInput")
+					
 				},
 			) {
 				LazzyRow(Mod.centerV) {
