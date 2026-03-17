29426537f7d7d284589052c2dfb1b97b3fcfe5dc
potatochad <163626408+potatochad@users.noreply.github.com>
2026-03-17 13:02:48 +0100

    Update Props.kt

--- app/src/main/java/com/productivity/wind/Screens/Props.kt
+++ app/src/main/java/com/productivity/wind/Screens/Props.kt
@@ -81,7 +81,6 @@ fun NavGraphBuilder.ScreenNav() {
     url("Web") { Web() }
     url("WebKeywords") { WebKeywords() }
 	url("WebHome") { WebHome() }
-	url("WebInput") { WebInput() }
 	url("WebWordConfigure/{WordId}") { 
 		var x: Str = it.url("WordId") ?: ""
 		WebWordConfigure(x) 
