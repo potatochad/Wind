e476eb418860591be7d151830eda5ec38352141f
potatochad <163626408+potatochad@users.noreply.github.com>
2026-03-22 19:34:46 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,6 @@ plugins {
 }
 
 android {
-
-    
     namespace = "com.productivity.wind"
 
     compileSdk = 36
@@ -75,6 +73,7 @@ ktlint {
 
 
 dependencies {
+    implementation("com.github.pChochura:richtext-compose:1.3.2")
     implementation("org.apache.commons:commons-math3:3.6.1")
 
 
