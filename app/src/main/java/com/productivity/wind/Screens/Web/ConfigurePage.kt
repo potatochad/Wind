3ec2229e0c364292fa59586a06d68e613a59a5d4
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-01 13:21:47 +0300

    Update ConfigurePage.kt

--- app/src/main/java/com/productivity/wind/Screens/Web/ConfigurePage.kt
+++ app/src/main/java/com/productivity/wind/Screens/Web/ConfigurePage.kt
@@ -68,7 +68,7 @@ fun WebKeywords() {
             LazyCard(
 				modUI = Mod.space(10),
 				modCard = Mod.space(10).maxW().click {    
-					goTo("WebWordConfigure/${it.id}")
+					if (!it.locked) goTo("WebWordConfigure/${it.id}")
 				},
 			) { 
                 LazzyRow {
