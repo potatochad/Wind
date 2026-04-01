edd1cab64af229a07dcd3468db4b6b0d4386e88f
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-01 22:37:32 +0300

    Update Browser.kt

--- app/src/main/java/com/productivity/wind/Imports/UI_visible/Browser.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI_visible/Browser.kt
@@ -141,6 +141,8 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.material3.pulltorefresh.*
 import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
 
+import com.productivity.wind.Imports.Utils.Browser.*
+
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun WebXml(web: WebController) {
