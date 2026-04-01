6f60a152abe459ba165383bacfd1b6a0ad17ceff
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-01 22:35:48 +0300

    Update WebJs.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/Browser/WebJs.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/Browser/WebJs.kt
@@ -1,1 +1,30 @@
+package com.productivity.wind.Imports.Utils.Browser
+ 
+import android.annotation.SuppressLint
+import android.content.*
+import androidx.compose.runtime.*
+import androidx.activity.compose.*
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.compose.ui.viewinterop.*
+import android.view.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import com.productivity.wind.*
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.productivity.wind.R
+import com.productivity.wind.Imports.*
+import android.webkit.*
+import android.graphics.*
+import kotlinx.coroutines.*
+import com.productivity.wind.Imports.Utils.*
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+
+
+
+
 
