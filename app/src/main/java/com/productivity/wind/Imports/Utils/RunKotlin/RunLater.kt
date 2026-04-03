ac63ff31351a874fcc398a7baf18f789237d3d7a
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-03 17:26:09 +0300

    Update RunLater.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/RunKotlin/RunLater.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/RunKotlin/RunLater.kt
@@ -3,12 +3,22 @@ package com.productivity.wind.Imports.Utils.RunKotlin
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
 
+val jsActions = mList<Str>()
+
 fun RunJs(script: Str): Any? {
     val ctx = Context.enter()
     ctx.optimizationLevel = -1
     val scope = ctx.initStandardObjects()
     return ctx.evaluateString(scope, script, "<cmd>", 1, null)
 }
+
+//test: var x=5
+//clickbutton
+//5h later x=6
+
+
+
+
 
 
 
