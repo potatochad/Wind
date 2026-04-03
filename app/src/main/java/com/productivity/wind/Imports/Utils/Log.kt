ded86d2a0e969c00325d972a64ea4e38761029ed
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-03 09:28:01 +0300

    Update Log.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/Log.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/Log.kt
@@ -1,1 +1,270 @@
+package com.productivity.wind.Imports.Utils
+ 
+import android.annotation.SuppressLint
+import timber.log.Timber
+import java.text.*
+import android.app.usage.UsageStatsManager
+import androidx.compose.foundation.interaction.*
+import android.app.*
+import androidx.core.app.*
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.unit.*
+import androidx.compose.material3.*
+import androidx.compose.foundation.text.*
+import androidx.compose.ui.text.input.*
+import androidx.compose.runtime.*
+import android.os.*
+import android.content.*
+import android.util.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.text.font.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import kotlinx.coroutines.*
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.jvm.isAccessible
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlin.reflect.*
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import android.net.Uri
+import android.widget.Toast
+import com.productivity.wind.*
+import java.util.UUID
+import java.lang.reflect.Type
+import kotlin.collections.*
+import android.content.*
+import java.lang.reflect.ParameterizedType
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.res.painterResource
+import android.content.Intent
+import java.time.LocalDate
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.productivity.wind.Screens.*
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.layout.*
+import android.graphics.drawable.Drawable
+import android.content.pm.*
+import com.productivity.wind.Imports.*
+import java.util.*
+import com.productivity.wind.R
+import kotlin.reflect.full.*
+import androidx.compose.ui.focus.*
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.gestures.*
+import androidx.compose.ui.input.pointer.*
+import java.io.*
+import androidx.compose.ui.geometry.*
+import androidx.compose.ui.text.style.*
+import androidx.compose.foundation.lazy.*
+import java.util.*
+import kotlin.concurrent.*
+import androidx.annotation.RequiresApi
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.core.view.*
+import kotlin.reflect.*
+import kotlin.reflect.jvm.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import androidx.compose.foundation.text.selection.*
+import kotlin.system.*
+import androidx.navigation.*
+import android.webkit.*
+import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler
+import org.jetbrains.kotlin.cli.common.ExitCode
+import com.productivity.wind.Imports.Utils.*
+import android.location.*
+import androidx.core.content.*
+import androidx.compose.ui.text.*
+import androidx.navigation.compose.*
+import android.util.*
+import com.google.android.gms.maps.model.*
+import com.google.android.gms.location.*
+import android.content.*
+import android.net.*
+import androidx.lifecycle.*
+import kotlinx.coroutines.flow.*
+import androidx.compose.ui.window.*
+import com.productivity.wind.Imports.UI_visible.*
+import android.os.Process.*
+import android.content.ClipData
+import android.content.ClipboardManager
+
+
+         
+fun Any?.log(x: Str){
+	log("$x: [ $this ]", 400)
+}
+fun Any?.Vlog(x: Str){
+	Vlog("$x: [ $this ]", 400)
+}
+fun log(message: Str, int: Int = 200) {
+    var msg = message.replace("\n", " | ").take(int)
+    if (msg.length >= int) {msg += " ..."}
+
+    Log.e("[bad]", msg)
+}
+private var lastToast: Toast? = null
+
+fun Vlog(msg: Str, maxInt: Int = 200, special: Str = "none", delayLevel: Int = 0) {
+    val delayMs = (delayLevel.coerceIn(0, 100)) * 30L // Example: Level 2 = 60ms
+
+    if (special.equals("one", true)) {
+        lastToast?.cancel()
+    }
+
+	log(msg, maxInt)
+
+
+    Handler(Looper.getMainLooper()).postDelayed({
+        val toast = Toast.makeText(App, msg, Toast.LENGTH_SHORT)
+        lastToast = toast
+        toast.show()
+    }, delayMs)
+}
+
+
+
+fun MeasureWaitLag(title: Str, block: Wait) {
+    val start = System.currentTimeMillis()
+	Do { block() }
+    val end = System.currentTimeMillis()
+    log("[$title]: code took ${end - start} ms")
+}
+@Composable
+fun MeasureUILag(title: Str, block: ui) {
+    val start = System.currentTimeMillis()
+	block()
+    val end = System.currentTimeMillis()
+    log("[$title]: code took ${end - start} ms")
+}
+fun <T> MeasureLag(title: String, block: () -> T): T {
+    val start = System.currentTimeMillis()
+
+    val result = block()   // run and capture result
+
+    val end = System.currentTimeMillis()
+    log("[$title]: code took ${end - start} ms")
+
+    return result
+}
+fun MeasureLagNoReturn(title: String, block: () -> Unit) {
+    val start = System.currentTimeMillis()
+
+    block()   // just run it, ignore return
+
+    val end = System.currentTimeMillis()
+    log("[$title]: code took ${end - start} ms")
+}
+
+
+fun getMyAppLogs() {
+	Thread {
+		val pid = android.os.Process.myPid()
+		val process = Runtime.getRuntime().exec("logcat --pid=$pid *:W")
+		val reader = BufferedReader(InputStreamReader(process.inputStream))
+
+		reader.forEachLine { line ->
+			val s = line.replace(Regex("""^\d{2}-\d{2}\s+|\s+\d+\s+\d+\s+"""), " ").takeLast(3000)
+			if ("ApkAssets: Deleting" in s) return@forEachLine
+			if ("WindowOnBackDispatcher" in s) return@forEachLine
+			if (" W " in s) return@forEachLine
+
+			val last = Bar.logs.lastOrNull()
+
+			if (last != s){
+				Bar.logs.add(s)
+			} else {
+				if ("[bad]" in s) {
+					Bar.logs.add(s)
+				}
+			}
+
+			
+		}
+	}.start()
+}
+
+fun captureAppCrashes() {
+    Thread {
+        try {
+            val process = Runtime.getRuntime().exec("logcat *:E") 
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+
+			val currentTime = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
+
+
+            reader.forEachLine { line ->
+				val s = line.replace(Regex("""^\d{2}-\d{2}\s+|\s+\d+\s+\d+\s+"""), " ").takeLast(3000)
+
+				var logTime = s.takeLast(5) //09:25
+				
+                
+                if (s.contains("FATAL EXCEPTION") && s.contains(BuildConfig.APPLICATION_ID) && logTime == currentTime) {
+                    Bar.logs.add(s)
+                }
+            }
+        } catch (e: Exception) {
+			Vlog("exception trigerred")
+            e.printStackTrace()
+        }
+    }.start()
+}
+
+
+fun folder(folderName: Str): File {
+    val folder = File(App.filesDir, folderName)
+    if (!folder.exists()) {
+        folder.mkdirs()
+    }
+    return folder
+}
+
+
+
+
+
+
+
+
 
