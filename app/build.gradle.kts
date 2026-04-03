963f728e2d20fbdd2f682d1dcaa93ba2d2679bf5
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-03 12:37:21 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,57 +73,64 @@ ktlint {
 
 
 dependencies {
-    implementation("org.apache.commons:commons-math3:3.6.1")
-
-
-    implementation("javax.inject:javax.inject:1")
-
-    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
-
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.media:media:1.7.1")
-
-
-    implementation("com.google.maps.android:android-maps-utils:3.19.0")
-    implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("com.google.maps.android:maps-compose:6.9.0")
-    implementation("com.google.android.gms:play-services-maps:18.2.0") 
-    implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("org.mozilla:rhino:1.7.15")
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.1.10")
     compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.1.10")
-    implementation("androidx.compose.foundation:foundation:1.9.2")
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("com.github.skydoves:balloon:1.6.13")
-    implementation("androidx.compose.ui:ui")
-    implementation("com.google.accompanist:accompanist-drawablepainter:0.37.3")
-    implementation(platform(libs.androidx.compose.bom))
-    implementation("androidx.compose.foundation:foundation")
-    implementation("com.google.accompanist:accompanist-navigation-animation:0.34.0")
-    implementation("androidx.datastore:datastore-preferences:1.1.0")
-    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jsr223:2.1.10")
     implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.10")
-    implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("androidx.compose.material3:material3:1.3.2")
-    implementation("androidx.compose.material3:material3-window-size-class:1.3.2")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
-    implementation("androidx.activity:activity-compose:1.9.2")
-    implementation("androidx.compose.runtime:runtime-livedata:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    // AndroidX Core & Support
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.datastore:datastore-preferences:1.1.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
-    implementation("androidx.compose.material3:material3:1.3.2")
-    implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+
+    // Compose
+    implementation(platform(libs.androidx.compose.bom))
     implementation("androidx.compose.ui:ui:1.7.0")
     implementation("androidx.compose.ui:ui-graphics:1.7.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("androidx.compose.foundation:foundation:1.9.2")
+    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.2")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.0")
+    implementation("androidx.navigation:navigation-compose:2.7.0")
+
+    // Google / Play Services / Maps
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.maps.android:android-maps-utils:3.19.0")
+    implementation("com.google.maps.android:maps-compose:6.9.0")
+
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
+    implementation("com.google.accompanist:accompanist-drawablepainter:0.37.3")
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.34.0")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0")
+
+    // Networking / Utils
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.apache.commons:commons-math3:3.6.1")
+    implementation("org.mozilla:rhino:1.7.15")
+    implementation("javax.inject:javax.inject:1")
     implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("com.github.skydoves:balloon:1.6.13")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
+    implementation("com.google.android.material:material:1.13.0")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
+
+
+
+
