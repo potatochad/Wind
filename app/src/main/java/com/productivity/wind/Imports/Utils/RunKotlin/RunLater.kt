6474ce3cd6b70b77d660fe539e1452153bcce854
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-03 12:06:25 +0300

    Update RunLater.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/RunKotlin/RunLater.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/RunKotlin/RunLater.kt
@@ -1,1 +1,31 @@
+import java.io.File
+import javax.script.ScriptEngineManager
+
+var MagixBox = 0
+val file = File("magix.kts")
+
+// Schedule code
+fun scheduleCode(code: String) {
+    file.writeText(code)
+}
+
+// Execute saved code
+fun executeSavedCode() {
+    if (!file.exists()) return
+    val engine = ScriptEngineManager().getEngineByExtension("kts")
+    engine.put("MagixBox", MagixBox) // inject variable
+    engine.eval(file.readText())
+    MagixBox = engine.get("MagixBox") as Int
+}
+
+// Example
+fun main() {
+    println("Before: $MagixBox") // 0
+    scheduleCode("MagixBox = 42")
+    executeSavedCode()
+    println("After: $MagixBox")  // 42
+}
+
+
+
 
