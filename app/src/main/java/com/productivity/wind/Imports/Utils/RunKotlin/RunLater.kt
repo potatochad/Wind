a613343da67ac22a479fcc69cd86613d1abbef58
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-03 17:14:50 +0300

    Update RunLater.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/RunKotlin/RunLater.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/RunKotlin/RunLater.kt
@@ -3,7 +3,7 @@ package com.productivity.wind.Imports.Utils.RunKotlin
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
 
-fun runJs(script: String): Any? {
+fun RunJs(script: Str): Any? {
     val ctx = Context.enter()
     ctx.optimizationLevel = -1
     val scope = ctx.initStandardObjects()
