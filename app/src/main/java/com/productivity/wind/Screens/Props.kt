09638f24bbe443bffd29a330b9462cea65032ef2
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-10 08:50:28 +0300

    Update Props.kt

--- app/src/main/java/com/productivity/wind/Screens/Props.kt
+++ app/src/main/java/com/productivity/wind/Screens/Props.kt
@@ -113,6 +113,7 @@ fun Menu() {
             title = "Support",
             onClick = { SendEmail(); menu = no },
         )
+		move(10)
         LazyItem(
             icon = { Icon.Landscape() },
             title = "Settings",
