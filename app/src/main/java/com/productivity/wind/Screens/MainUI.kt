ee3674e36443764ff281190f9ad1004d0b5cf182
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-11 08:35:25 +0300

    Update MainUI.kt

--- app/src/main/java/com/productivity/wind/Screens/MainUI.kt
+++ app/src/main/java/com/productivity/wind/Screens/MainUI.kt
@@ -91,7 +91,7 @@ fun Main() {
 					
 					LazyCard(
 						modUI = Mod.space(start = 8),
-						modCard = Mod.space(h=8, w=10).maxW().click {    
+						modCard = Mod.space(x=8, y=10).maxW().click {    
 							goTo("ToDo/${it.id}")
 						},
 					) { 
@@ -122,7 +122,7 @@ fun Main() {
 			}) { 						
 				LazyCard(
 					modUI = Mod.space(start = 8),
-					modCard = Mod.space(h=8, w=10).maxW().click {    
+					modCard = Mod.space(x=8, y=10).maxW().click {    
 						goTo("ToDo/${it.id}")
 					},
 				) { 
