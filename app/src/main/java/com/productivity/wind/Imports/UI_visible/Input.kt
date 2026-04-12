b76ef4c5428254113ce13c3715a788a302c8ac47
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-12 22:33:34 +0200

    Update Input.kt

--- app/src/main/java/com/productivity/wind/Imports/UI_visible/Input.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI_visible/Input.kt
@@ -1,5 +1,6 @@
 package com.productivity.wind.Imports.UI_visible
 
+import com.productivity.wind.Imports.Utils.Str.*
 import android.annotation.SuppressLint
 import timber.log.Timber
 import java.text.*
