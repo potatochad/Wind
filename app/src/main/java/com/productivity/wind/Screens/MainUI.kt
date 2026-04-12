c594cb26a7a0fd717f8fcb86ce28491b30f6246a
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-12 22:53:15 +0200

    Update MainUI.kt

--- app/src/main/java/com/productivity/wind/Screens/MainUI.kt
+++ app/src/main/java/com/productivity/wind/Screens/MainUI.kt
@@ -1,6 +1,6 @@
 package com.productivity.wind.Screens
 
-import com.productivity.wind.Imports.Utils.Str.*
+import com.productivity.wind.Imports.Utils.String.*
 import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
