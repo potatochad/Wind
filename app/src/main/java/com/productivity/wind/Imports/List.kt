93ee9eb232961a76b20a8fa28a7719280bd5bcc6
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-12 22:48:15 +0200

    Update List.kt

--- app/src/main/java/com/productivity/wind/Imports/List.kt
+++ app/src/main/java/com/productivity/wind/Imports/List.kt
@@ -1,6 +1,6 @@
 package com.productivity.wind.Imports
 
-import com.productivity.wind.Imports.Utils.Str.*
+import com.productivity.wind.Imports.Utils.String.*
 import com.productivity.wind.Imports.Utils.*
 import android.app.usage.UsageStatsManager
 import android.app.*
