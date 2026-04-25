c4d919565bc0ff0828e1d9fe298f3687c4645c03
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-25 08:08:11 +0200

    Update UIStr.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/String/UIStr.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/String/UIStr.kt
@@ -192,7 +192,7 @@ fun UIStr.strStyle(x: StrStyle): UIStr {
     }
 }
 
-fun Any.txt(update: StrStyle.() -> StrStyle = { this }): UIStr = UIText(this, (this.style.update()))
+fun Any.txt(update: StrStyle.() -> StrStyle = { this }): UIStr = UIText(this, (toUIStr(this).style.update()))
 
 fun Any.size(x: Int) = txt { copy(fontSize = x.sp) }
 fun Any.size(x: Float) = txt { copy(fontSize = x.sp) }
