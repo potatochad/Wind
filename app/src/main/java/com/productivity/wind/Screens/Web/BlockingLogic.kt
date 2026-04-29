c12f99059d8e3d26a66fba523a515da04647e824
potatochad <163626408+potatochad@users.noreply.github.com>
2026-04-29 07:41:24 +0200

    Update BlockingLogic.kt

--- app/src/main/java/com/productivity/wind/Screens/Web/BlockingLogic.kt
+++ app/src/main/java/com/productivity/wind/Screens/Web/BlockingLogic.kt
@@ -48,7 +48,7 @@ fun BlockingLogic(web: WebController){
 	web.doUpdateVisitedHistory { url, isReload ->
 		Bar.Url = url ?: "https://www.google.com"
 
-		if (Bar.Url.hasAny("melrobbins.com/podcast/", "mfmpod.com")) {
+		if (Bar.Url.hasAny("melrobbins.com/podcast/", "mfmpod.com", "youtube.com")) {
 			//Allow
 		} else {
 			Block()
