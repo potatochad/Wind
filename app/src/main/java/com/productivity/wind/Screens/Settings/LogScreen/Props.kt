7063ba69945eccf035267afd1451d03c2ea38f81
potatochad <163626408+potatochad@users.noreply.github.com>
2026-05-08 09:47:34 +0200

    Update Props.kt

--- app/src/main/java/com/productivity/wind/Screens/Settings/LogScreen/Props.kt
+++ app/src/main/java/com/productivity/wind/Screens/Settings/LogScreen/Props.kt
@@ -1,1 +1,31 @@
+package com.productivity.wind.Screens
+
+import com.productivity.wind.Imports.Utils.String.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.delay
+import android.service.notification.NotificationListenerService
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.*
+import com.productivity.wind.Imports.*
+import com.productivity.wind.*
+import com.productivity.wind.Imports.Utils.*
+import androidx.compose.ui.unit.*
+import androidx.compose.foundation.shape.*
+import androidx.compose.ui.text.style.*
+import androidx.compose.ui.*
+import androidx.compose.foundation.lazy.*
+import com.google.android.gms.maps.model.*
+import com.google.maps.android.compose.*
+import com.productivity.wind.Imports.UI_visible.*
+import android.os.*
+import androidx.compose.ui.text.input.*
+import androidx.compose.material3.pulltorefresh.*
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator      
+
 
