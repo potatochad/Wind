63df1e0fefd75decdd84ed046e3de178d6a70247
potatochad <163626408+potatochad@users.noreply.github.com>
2026-05-08 23:09:39 +0200

    Update WebJs.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/Browser/WebJs.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/Browser/WebJs.kt
@@ -52,7 +52,7 @@ fun Any?.jsFun(code: Str, callback: ((Str?) -> Unit)? = null) {
 //RUNS BEFORE PAGE LOADS
 fun Any?.importsJS() {
     this.jsFun(
-        
+        getTextAsset("ImportsJS.js")
     )
 }
 
