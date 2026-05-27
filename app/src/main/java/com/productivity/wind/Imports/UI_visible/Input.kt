5dfbdba85069b23c17f930ecab8c1ec0271db669
potatochad <163626408+potatochad@users.noreply.github.com>
2026-05-27 22:09:36 +0200

    Update Input.kt

--- app/src/main/java/com/productivity/wind/Imports/UI_visible/Input.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI_visible/Input.kt
@@ -145,8 +145,8 @@ fun BasicInput(
 	oneLine: Bool = yes,
     Do: Do_<InputField> = {},
 ) {
-	val baseMod = Mod.w(60).h(26).space(8, 4).background(inputColor, shape = RoundedCornerShape(4.dp)).space(start = 3, top = 5)             
-	
+	val baseMod = Mod.w(60).h(26).background(inputColor, shape = RoundedCornerShape(4.dp)).space(start = 3, top = 5)             
+	// space(8, 4)
 	
 	BasicTextField(
 		value = Field.it,
