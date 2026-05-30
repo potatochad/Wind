9b67a400bd05525a0a52a55ffbbaa243fe196d2a
potatochad <163626408+potatochad@users.noreply.github.com>
2026-05-30 08:45:45 +0200

    Update DateAndTime.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/DateAndTime.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/DateAndTime.kt
@@ -193,7 +193,19 @@ data class Schedule(
 
 
 
+class Vtimer(private val name: Str = "") {
 
+    private val start = System.nanoTime()
+
+    fun end() {
+        val ms = String.format(
+            "%.3f",
+            (System.nanoTime() - start) / 1_000_000.0
+        )
+
+        Vlog("${if (name.empty) "It" else name} took ${ms}ms")
+    }
+}
 
 
 
