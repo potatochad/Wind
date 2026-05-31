3c58761324efe9f3ce93f68bbfd0c2af0b522422
potatochad <163626408+potatochad@users.noreply.github.com>
2026-05-31 21:51:45 +0200

    Update Settings.kt

--- app/src/main/java/com/productivity/wind/Screens/Settings.kt
+++ app/src/main/java/com/productivity/wind/Screens/Settings.kt
@@ -252,7 +252,7 @@ fun LogsScreen() {
 			) {
 				LazyColumn(
 					state = scroll,
-					modifier = Mod.w(maxW).border(2.dp, red)      
+					modifier = Mod.w(maxW)   
 				) {
 					items(Logs) { LogTxt(it) }
 				}
