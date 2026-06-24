53db62c37e7086556bc360ed284f7c5ce5883b77
potatochad <163626408+potatochad@users.noreply.github.com>
2026-06-24 07:56:06 +0200

    Update Browser.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/Browser/Browser.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/Browser/Browser.kt
@@ -93,7 +93,7 @@ class WebController(
 				//daren daily
 				//tony robbins
 				view?.youtubeFilter(
-					allowOnly = listOf("disipline"),
+					allowOnly = listOf("discipline"),
 					block = listOf("MrBeast", "McYum", "Mark Rober", " TABS ", " Aliens ", "Technoblade", "Skeppy", "Grian", "Spifey", "Minecraft", "Speedrun", "Mr Bean", "POV", "m views", "redstone", "command blocks", "00 IQ", "poly bridge", "iswho", "rageplaysgames", "leowook", "shalz", "chess", " vs ", " vs. ", "reddoons", "real civil engineer", "cappy army", "iwantcheckmate", "Can I", "but everyone", " noob ", " OP ", " 1v1 ", " 3v1 ", " 2v1 ", ".io", "! ", " trackmania ", " 100% ", "Players", "Market crash", "king", "flood", "but all", "geometry dash", "just lost", "world box", "law by mike", "smartest", "baronVonGames", "...", " SMP ", "1000 days", "100 days")         
 				)
 				
