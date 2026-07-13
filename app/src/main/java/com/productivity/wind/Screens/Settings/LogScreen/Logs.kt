8dbe3b98aacb2b4876e3acc3e6460e1923e25e83
potatochad <163626408+potatochad@users.noreply.github.com>
2026-07-13 09:51:53 +0300

    Update Logs.kt

--- app/src/main/java/com/productivity/wind/Screens/Settings/LogScreen/Logs.kt
+++ app/src/main/java/com/productivity/wind/Screens/Settings/LogScreen/Logs.kt
@@ -1,5 +1,6 @@
 package com.productivity.wind.Screens.Settings.LogScreen
 
+import com.productivity.wind.Screens.*
 import com.productivity.wind.Imports.Utils.ToX.*
 import com.productivity.wind.Imports.Utils.String.*
 import androidx.compose.foundation.layout.*
