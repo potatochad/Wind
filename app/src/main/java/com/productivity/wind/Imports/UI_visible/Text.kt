b07f52f5398c32ec0551e53c987178b53435fa13
potatochad <163626408+potatochad@users.noreply.github.com>
2026-07-15 09:47:08 +0300

    Update Text.kt

--- app/src/main/java/com/productivity/wind/Imports/UI_visible/Text.kt
+++ app/src/main/java/com/productivity/wind/Imports/UI_visible/Text.kt
@@ -134,6 +134,29 @@ import android.content.ClipboardManager
 
 
 
+/*
+
+Text(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+    onTextLayout: ((TextLayoutResult) -> Unit)? = null,
+    style: TextStyle = LocalTextStyle.current
+)
+*/
+
 @Composable
 fun LogTxt(text: Str) {
     Text(
