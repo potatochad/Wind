5569d79dc86497a6d33e2b9d64130a3ae18ccf7a
potatochad <163626408+potatochad@users.noreply.github.com>
2026-07-17 18:45:46 +0300

    Update Log.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/Log/Log.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/Log/Log.kt
@@ -25,67 +25,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.CancellationException
 
 
-typealias Str = String
-typealias Bool = Boolean
-typealias Do = () -> Unit
-typealias Wait = suspend () -> Unit
-typealias mList<T> = MutableList<T>
-
-typealias ui = @Composable () -> Unit
-typealias ui_<T> = @Composable (T) -> Unit
-
-
-val Str.size: Int
-    get() = length
-fun Str.last(n: Int): Str = takeLast(n)
-fun Str.fromTo(start: Int, end: Int = this.size) = substring(start, end)
-fun <T> mList<T>.fromTo(start: Int, end: Int = size): mList<T> = subList(start, end)
-
-fun <T> mList<T>.keep(max: Int) {
-    if (size > max) {
-        fromTo(0, size - max).clear()
-    }
-}
-
-@Composable
-fun <T> r(x: () -> T) = remember { x() }
-fun <T> m(value: T) = mutableStateOf(value)
-@Composable
-fun <T> r(x: T) = r { m(x) }
-@Composable
-inline fun <T> r(vararg keys: Any?, crossinline calc: () -> T): T = remember(*keys, calculation = calc)
-
-
-fun <T> mList() = mutableStateListOf<T>()
-
-
-fun Do(eLog: Str="", onError: Wait ={}, Do: Wait) {
-	var whereCalled by m("")
-	appScope.launch {
-		try {
-			whereCalled = callerId(2)
-			Do()
-		} catch (e: CancellationException) {
-			throw e
-		} catch (e: Exception) {
-			Vlog("Do error, $eLog: ${e.message}, $whereCalled")
-			onError()
-		}
-	} 
-}
-fun callerId(depth: Int = 0): Str {
-    val stack = Throwable().stackTrace
-
-    if (depth !in stack.indices) {
-        return "invalid-depth"
-    }
-
-    val it = stack[depth]
-
-    return "${it.fileName}-${it.className.substringAfterLast('.')}-${it.methodName}"
-}
-
-
 
 fun Any?.vlog(x: Str){
 	Vlog("$x: [ $this ]", 800)
