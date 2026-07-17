2d05820bdd90b66f6d43b9d1a5db3d25c41e9306
potatochad <163626408+potatochad@users.noreply.github.com>
2026-07-17 11:59:00 +0300

    Update Log.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/Log/Log.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/Log/Log.kt
@@ -3,10 +3,6 @@
 
 package com.productivity.wind.Imports.Utils.Log
 
-import com.productivity.wind.Imports.Utils.ToX.*
-import com.productivity.wind.Imports.Utils.String.*
-import com.productivity.wind.Imports.Utils.*
-
 import com.productivity.wind.Bar
 
 import android.os.*
