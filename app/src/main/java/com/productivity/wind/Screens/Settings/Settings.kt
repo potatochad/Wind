56ce43e3a99a6f28c60d703c518a85e58599a417
potatochad <163626408+potatochad@users.noreply.github.com>
2026-07-20 21:50:17 +0300

    Update Settings.kt

--- app/src/main/java/com/productivity/wind/Screens/Settings/Settings.kt
+++ app/src/main/java/com/productivity/wind/Screens/Settings/Settings.kt
@@ -1,5 +1,6 @@
 package com.productivity.wind.Screens.Settings
 
+import com.productivity.wind.Imports.Utils.AppsAndDevice.*
 import com.productivity.wind.Imports.Utils.NavControl.*
 import com.productivity.wind.Screens.*
 import com.productivity.wind.Imports.Utils.ToX.*
