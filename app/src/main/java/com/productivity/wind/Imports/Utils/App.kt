1c372405319137fcd9663426625540db27f7c0c7
potatochad <163626408+potatochad@users.noreply.github.com>
2026-07-20 22:24:18 +0300

    Update App.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/App.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/App.kt
@@ -205,20 +205,7 @@ fun ComposeCanBeTiny(ui: ui) {
 
 
 
-//THISS MIGHT BE INVERTING COLOURS (BASED ON USER SETTINGS THAT ACTUALLY MAKING DEVICE MONOCROME, (IDKKK)
-fun DeviceGray(enabled: Bool = true) {
-    Settings.Secure.putInt(
-        App.contentResolver,
-        "accessibility_display_daltonizer",
-        0
-    )
 
-    Settings.Secure.putInt(
-        App.contentResolver,
-        "accessibility_display_daltonizer_enabled",
-        if (enabled) 1 else 0
-    )
-}
 
 
 
