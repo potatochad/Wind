de2d7b16fecf0eaf5708c4a42ae4c2ff43a4bbdc
potatochad <163626408+potatochad@users.noreply.github.com>
2026-07-20 19:25:33 +0300

    Update MainUI.kt

--- app/src/main/java/com/productivity/wind/Screens/MainUI.kt
+++ app/src/main/java/com/productivity/wind/Screens/MainUI.kt
@@ -1,5 +1,6 @@
 package com.productivity.wind.Screens
 
+import com.productivity.wind.Imports.Utils.AppsAndDevice.*
 import com.productivity.wind.Imports.Utils.NavControl.*
 import com.productivity.wind.Imports.Utils.ToX.*
 import com.productivity.wind.Imports.Utils.String.*
