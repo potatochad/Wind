db60a94226aeb537a368633f3d6f54dcc9a78e4f
potatochad <163626408+potatochad@users.noreply.github.com>
2026-07-23 13:42:27 +0300

    Update Data.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/Data.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/Data.kt
@@ -253,13 +253,13 @@ fun <T> s(
 	var badId by m(no)
 	
 	delegate
-		.onBuild{ prop, id ->
+		.onBuild{ prop, id, delegateIt ->
 			goodId = "$id: $idExtra"
 			
 			badId = idList.has(goodId)
 			idList.add(goodId)
 			if (badId) Vlog("Duplicate id detected: $goodId")
-			else delegate.it = AppData.get(goodId, default)
+			else delegateIt = AppData.get(goodId, default)
 		}
 		.onSet{ prop, id, newValue ->
 			if (!badId) AppData.put(goodId, newValue)
