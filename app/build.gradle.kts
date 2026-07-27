c00dd8a0f01a89b43be948ce2f9267e16f69f696
potatochad <163626408+potatochad@users.noreply.github.com>
2026-07-27 21:08:16 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.jetbrainsCompose)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.serialization)
 }
