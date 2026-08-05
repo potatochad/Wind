f7050193d86da54c58766966f8340d4a31eaef13
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-05 23:10:20 +0300

    Update List.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/List.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/List.kt
@@ -226,15 +226,10 @@ fun < T : LazyData> TrackList(
             }
 
             theList = EasyList( if (savedItems.notEmpty) savedItems else defaultItems )
-                .onAdd{
+                .onAdd = {
                     it.prepare() 
                 }
-                .onClear{
-                    
-                }
-                .onRemove{
-                    
-                }
+                /*.onClear{} .onRemove{}*/
                 
         
             theList?.each { index, item -> 
