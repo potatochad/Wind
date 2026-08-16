fc235572c59267d4fc6722ac1dafdccc0a1106ee
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-16 22:39:22 +0300

    Update By.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/Renames/By.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/Renames/By.kt
@@ -1,1 +1,181 @@
+package com.productivity.wind.Imports.Utils.Renames
+
+import com.productivity.wind.Imports.Utils.SaveData.*
+import com.productivity.wind.Imports.Utils.AppsAndDevice.*
+import com.productivity.wind.Imports.Utils.NavControl.*
+import com.productivity.wind.Imports.Utils.ToX.*
+import com.productivity.wind.Imports.Utils.String.*
+import android.annotation.SuppressLint
+import timber.log.Timber
+import java.text.*
+import android.app.usage.UsageStatsManager
+import androidx.compose.foundation.interaction.*
+import android.app.*
+import androidx.core.app.*
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.unit.*
+import androidx.compose.material3.*
+import androidx.compose.foundation.text.*
+import androidx.compose.ui.text.input.*
+import androidx.compose.runtime.*
+import android.os.*
+import android.content.*
+import android.util.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.text.font.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import kotlinx.coroutines.*
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.jvm.isAccessible
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlin.reflect.*
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import android.net.Uri
+import android.widget.Toast
+import com.productivity.wind.*
+import java.util.UUID
+import java.lang.reflect.Type
+import kotlin.collections.*
+import android.content.*
+import java.lang.reflect.ParameterizedType
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.res.painterResource
+import android.content.Intent
+import java.time.LocalDate
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.productivity.wind.Screens.*
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.layout.*
+import android.graphics.drawable.Drawable
+import android.content.pm.*
+import com.productivity.wind.Imports.*
+import java.util.*
+import com.productivity.wind.R
+import kotlin.reflect.full.*
+import androidx.compose.ui.focus.*
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.gestures.*
+import androidx.compose.ui.input.pointer.*
+import java.io.*
+import androidx.compose.ui.geometry.*
+import androidx.compose.ui.text.style.*
+import androidx.compose.foundation.lazy.*
+import java.util.*
+import kotlin.concurrent.*
+import androidx.annotation.RequiresApi
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.*
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.core.view.*
+import kotlin.reflect.*
+import kotlin.reflect.jvm.*
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import androidx.compose.foundation.text.selection.*
+import kotlin.system.*
+import androidx.navigation.*
+import android.webkit.*
+import org.jetbrains.kotlin.cli.jvm.K2JVMCompiler
+import org.jetbrains.kotlin.cli.common.ExitCode
+import com.productivity.wind.Imports.Utils.*
+import androidx.compose.ui.text.*
+import com.productivity.wind.Imports.UI_visible.*
+import java.time.*
+import java.time.format.*
+import android.view.*
+import androidx.core.view.*
+import android.widget.RemoteViews
+import androidx.core.net.toUri
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.channels.Channel
+
+
+class By<T>(value: T) : ByExtra<T>() {
+	var it by mState(value)
+	var delegateValue = mState(value)
+	private var id by mState("")
+
+	var gotOrSet by mState(no)
+	var onUsagefreeze by mState(no)
+	
+	private var onBuild: Do3_<ValVar, Str, mState_<T>> = { _, _, _ -> }
+    private var onGet: Do_<ValVar> = {}
+    private var onSet: Do3_<ValVar, Str, T> = { _, _, _ -> }
+	private var onFirstGetOrSet: Do3_<ValVar, Str, mState_<T>> = { _, _, _ -> }
+
+	fun onBuild(x: Do3_<ValVar, Str, mState_<T>>) = apply { onBuild = x }
+    fun onGet(x: Do_<ValVar>) = apply { onGet = x }
+    fun onSet(x: Do3_<ValVar, Str, T>) = apply { onSet = x }
+	fun onFirstGetOrSet(freeze: Bool = no, x: Do3_<ValVar, Str, mState_<T>>) = apply { 
+		onFirstGetOrSet = x
+		onUsagefreeze = freeze
+	}
+	
+	
+	
+    operator fun provideDelegate(thisRef: Any?, property: ValVar): By<T> {
+		id = property.name
+		onBuild(property, id, delegateValue)
+		it = delegateValue.it 
+        return this
+    }
+    operator fun getValue(thisRef: Any?, property: ValVar): T {
+		if (!gotOrSet && !onUsagefreeze) onFirstGetOrSet(property, id, delegateValue)
+		gotOrSet = yes
+		
+		onGet(property)
+		it = delegateValue.it 
+        return it
+    }
+    operator fun setValue(thisRef: Any?, property: ValVar, newValue: T) {
+		if (!gotOrSet && !onUsagefreeze) onFirstGetOrSet(property, id, delegateValue)
+		gotOrSet = yes
+		
+        it = newValue
+		delegateValue.it = it
+		onSet(property, id, it)
+    }
+}
+
 
