f35f427aa2ee8860377f5e748ce666e73d5100a2
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-19 20:18:10 +0300

    Update Data.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/Data.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/Data.kt
@@ -167,7 +167,7 @@ object AppData {
 	fun remove(id: Str) = dataEdit.remove(id).apply()
 
 	fun hasKey(x: Str) = prefs.hasKey(x)
-	//fun hasKey(key: Str) = prefs.all.keys.any { it.contains(key) }
+	fun findKeyPart(key: Str) = prefs.all.keys.any { it.contains(key) }
 
 
 	val json11 = Json { ignoreUnknownKeys = yes }
