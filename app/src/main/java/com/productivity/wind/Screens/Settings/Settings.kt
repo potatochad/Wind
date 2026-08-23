543935544e8060f207887924aa8f56e3b9c90b93
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-23 23:49:55 +0300

    Uncomment Log import in Settings.kt

--- app/src/main/java/com/productivity/wind/Screens/Settings/Settings.kt
+++ app/src/main/java/com/productivity/wind/Screens/Settings/Settings.kt
@@ -1,6 +1,6 @@
 package com.productivity.wind.Screens.Settings
 
-// import com.productivity.wind.Imports.Utils.Log.*
+import com.productivity.wind.Imports.Utils.Log.*
 import com.productivity.wind.Imports.Utils.Generic_list.*
 import com.productivity.wind.Imports.Utils.Renames.*
 import com.productivity.wind.Imports.Utils.SaveData.*
