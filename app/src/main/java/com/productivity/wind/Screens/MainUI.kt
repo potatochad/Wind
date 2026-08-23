35f34d184559ce6588c964bc21f72b1158b8b2f2
potatochad <163626408+potatochad@users.noreply.github.com>
2026-08-23 23:46:56 +0300

    Update MainUI.kt

--- app/src/main/java/com/productivity/wind/Screens/MainUI.kt
+++ app/src/main/java/com/productivity/wind/Screens/MainUI.kt
@@ -1,5 +1,6 @@
 package com.productivity.wind.Screens
 
+import com.productivity.wind.Imports.Utils.Log.*
 import com.productivity.wind.Imports.Utils.Generic_list.*
 import com.productivity.wind.Imports.Utils.Renames.*
 import com.productivity.wind.Imports.Utils.SaveData.*
