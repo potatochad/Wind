6d320022f731dc75b2cf033f3f1ecf5f9793f925
potatochad <163626408+potatochad@users.noreply.github.com>
2026-09-05 19:40:03 +0200

    Update Extensions.kt

--- app/src/main/java/com/productivity/wind/Screens/Settings/Extensions.kt
+++ app/src/main/java/com/productivity/wind/Screens/Settings/Extensions.kt
@@ -25,7 +25,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
-
+@Composable
+fun TestScreen() = CreativePlayStore()
 // ============================================================
 // ENTRY POINT
 // ============================================================
