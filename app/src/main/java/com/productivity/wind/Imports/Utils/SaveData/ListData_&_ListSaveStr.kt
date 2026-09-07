1e57f655c75f55a26d17abfdae2b11345ba9c2df
potatochad <163626408+potatochad@users.noreply.github.com>
2026-09-07 21:59:28 +0200

    Update ListData_&_ListSaveStr.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/ListData_&_ListSaveStr.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/ListData_&_ListSaveStr.kt
@@ -154,6 +154,11 @@ class ListSaveStr(listName: Str) {
 
     var strData: Str = ListData[listName]
 
+    // I COULD MAKE THIS PRODUCE MAP THAT THEN USEE LATER
+    //FOR LIKE VARS MATCH
+    // ‼️ HOWEVER I THINK BETTER IS PRODUCE GLOBAL MAPS OF MAPS
+    //As i already make a map while iterating through each stuff
+    //AND IN GENERAL would be nicerr
     private fun countVars(): Int {
         val start = strData.indexOf('{')
         val end = strData.lastIndexOf('}')
