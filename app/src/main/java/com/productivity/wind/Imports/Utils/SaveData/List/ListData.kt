d3ee9b48087b7559e4e77bd1f89000903b76f989
potatochad <163626408+potatochad@users.noreply.github.com>
2026-09-23 21:37:44 +0200

    Remove unused import for Renames
    Removed unused import statement for Renames.

--- app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/List/ListData.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/SaveData/List/ListData.kt
@@ -4,7 +4,6 @@ import com.productivity.wind.Imports.Utils.VarVal.*
 import com.productivity.wind.Imports.Utils.SaveData.*
 import com.productivity.wind.Imports.Utils.Log.*
 import com.productivity.wind.Imports.Utils.Generic_list.*
-import com.productivity.wind.Imports.Utils.Renames.*
 import com.productivity.wind.Imports.Utils.AppsAndDevice.*
 import com.productivity.wind.Imports.Utils.NavControl.*
 import com.productivity.wind.Imports.Utils.ToX.*
