f2d16b40c7979cf95b2605e1a4d0920e33607eb6
potatochad <163626408+potatochad@users.noreply.github.com>
2026-09-25 23:05:51 +0200

    Update StrToValue.kt

--- app/src/main/java/com/productivity/wind/Imports/Utils/ToX/StrToValue.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/ToX/StrToValue.kt
@@ -1,17 +1,60 @@
-class StrToValue {
+package com.productivity.wind.Imports.Utils.VarVal
 
-    fun convert(type: Str, raw: Str): Any? {
-        val clazz = StrToClass(type) ?: return null
+import com.productivity.wind.Imports.Utils.SaveData.List.*
+import com.productivity.wind.Imports.Utils.Log.*
+import com.productivity.wind.Imports.Utils.Generic_list.*
+import com.productivity.wind.Imports.Utils.SaveData.*
+import com.productivity.wind.Imports.Utils.AppsAndDevice.*
+import com.productivity.wind.Imports.Utils.NavControl.*
+import com.productivity.wind.Imports.Utils.ToX.*
+import com.productivity.wind.Imports.Utils.String.*
+import android.annotation.SuppressLint
+import timber.log.Timber
+import java.text.*
+import android.app.usage.UsageStatsManager
+import androidx.compose.foundation.interaction.*
+import android.app.*
+import androidx.core.app.*
+import android.os.*
+import android.content.*
+import android.util.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import kotlinx.coroutines.*
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.jvm.isAccessible
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlin.reflect.*
+import android.widget.Toast
+import com.productivity.wind.*
+import java.util.UUID
+import java.lang.reflect.Type
+import kotlin.collections.*
+import android.content.*
+import java.lang.reflect.ParameterizedType
+import android.content.Intent
+import com.productivity.wind.Imports.*
+import com.productivity.wind.Imports.Utils.*
+import androidx.compose.ui.text.*
+import com.productivity.wind.Imports.UI_visible.*
 
-        return when {
-            isString(clazz) -> raw.removeSurrounding("\"")
-            isInteger(clazz) -> raw.toIntOrNull()
-            isBoolean(clazz) -> raw.toBooleanStrictOrNull()
-            isLong(clazz) -> raw.toLongOrNull()
-            isDouble(clazz) -> raw.toDoubleOrNull()
-            isFloat(clazz) -> raw.toFloatOrNull()
-            isEnum(clazz) -> getEnumValue(clazz, raw)
-            else -> null
-        }
+
+
+fun ClassToValue(type: Str, raw: Str): Any? {
+    val clazz = StrToClass(type) ?: return null
+
+    return when {
+        isString(clazz) -> raw.removeSurrounding("\"")
+        isInteger(clazz) -> raw.toIntOrNull()
+        isBoolean(clazz) -> raw.toBooleanStrictOrNull()
+        isLong(clazz) -> raw.toLongOrNull()
+        isDouble(clazz) -> raw.toDoubleOrNull()
+        isFloat(clazz) -> raw.toFloatOrNull()
+        isEnum(clazz) -> getEnumValue(clazz, raw)
+        else -> null
     }
 }
+
