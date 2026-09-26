8c47ecb7cbbf79c65b2f0004d8a94b300c514ed8
potatochad <163626408+potatochad@users.noreply.github.com>
2026-09-26 21:04:36 +0200

    Refactor getVarValue to use toValueOrNull

--- app/src/main/java/com/productivity/wind/Imports/Utils/VarVal/VarInfo.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/VarVal/VarInfo.kt
@@ -49,17 +49,7 @@ class VarInfoWorker(){
 	fun getVarValue(type: Str, raw: Str): Any? {
 		val clazz = StrToClass(type) ?: return null
 
-		return when {
-			isString(clazz) -> raw.removeSurrounding("\"")
-			isInteger(clazz) -> raw.toIntOrNull()
-			isBoolean(clazz) -> raw.toBooleanStrictOrNull()
-			isLong(clazz) -> raw.toLongOrNull()
-			isDouble(clazz) -> raw.toDoubleOrNull()
-			isFloat(clazz) -> raw.toFloatOrNull()
-			isEnum(clazz) -> raw.toEnumOrNull(clazz)
-			//isMyAppClass(clazz) -> getComplexValue(clazz, raw)
-			else -> null
-		}
+		return toValueOrNull(clazz, raw)
 	}
 
 
