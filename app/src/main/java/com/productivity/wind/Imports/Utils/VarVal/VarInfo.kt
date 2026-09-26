708a3a16fa96eb6d7d17b07186afc7595fe6cadb
potatochad <163626408+potatochad@users.noreply.github.com>
2026-09-26 22:03:22 +0200

    Fix getVarValue to use toValue instead of toValueOrNull

--- app/src/main/java/com/productivity/wind/Imports/Utils/VarVal/VarInfo.kt
+++ app/src/main/java/com/productivity/wind/Imports/Utils/VarVal/VarInfo.kt
@@ -48,7 +48,7 @@ class VarInfoWorker(){
 	
 	fun getVarValue(type: Str, raw: Str): Any? {
 		val clazz = StrToClass(type) ?: return null
-		val value = toValueOrNull(clazz, raw)
+		val value = toValue(clazz, raw)
 		if (value == null) Vlog("getVarValue error: $type, $raw, $clazz, $value")
 		return value
 	}
